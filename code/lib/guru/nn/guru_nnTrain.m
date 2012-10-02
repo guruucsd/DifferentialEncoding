d0a9b2a337e47f258e32c95f5cff7164df894cf3
Ben Cipollini <bcipolli@ucsd.edu>
2012-10-02 09:18:10 -0700

    moving xferfn logic to common codebase

--- code/lib/guru/nn/guru_nnTrain.m
+++ code/lib/guru/nn/guru_nnTrain.m
@@ -14,8 +14,17 @@ function [model,o_p] = guru_nnTrain(model,X,Y)
 %  o_p    : calculated output at LAST step.
 
   if (~isfield(model, 'TrainMode')), model.TrainMode = 'batch'; end;
-  try, startTime = toc; catch err, tic; startTime = toc; end; 
+  try, startTime = toc; catch err, tic; startTime = toc; end;
   
+  nUnits = size(model.Weights,1); nOutput = size(Y,1); nInput = size(X,1); nHidden = nUnits-nOutput-nInput;
+  if (isfield(model, 'linout') && model.linout && length(model.XferFn) ~= (nHidden+nOutput))
+    old_xferfn = model.XferFn;
+    model.XferFn = [model.XferFn*ones(1,nHidden) ones(1,nOutput)]; %linear hidden->output
+  elseif (length(model.XferFn)==2)
+    old_xferfn = model.XferFn;
+    model.XferFn = [model.XferFn(1)*ones(1,nHidden) model.XferFn(2)*ones(1,nOutput)];
+  end;
+
   switch (model.TrainMode)
       case 'batch' 
         if (nargout<2), [model]     = guru_nnTrain_batch(model,X,Y);
@@ -43,4 +52,11 @@ function [model,o_p] = guru_nnTrain(model,X,Y)
   model.trainTime(end+1)     = toc - startTime;
   model.avgErr(end+1)        = mean(model.err(end,:),2)/size(Y,1);
   model.trainingError(end+1) = sum(model.err(end,:));
-  
+
+    % Undo expansion of XferFn, for caching purposes
+  if (exist('old_xferfn','var'))
+      model.XferFn = old_xferfn;
+      clear('old_xferfn')
+  end;
+
+
