a7ef3e01e9eb72be7282a64187acc1222a566c01
Ben Cipollini <bcipolli@ucsd.edu>
2012-11-08 21:15:01 -0800

    updating parameters for remote running

--- scripts/34x25_gauss/sergent_1982/uber_sergent_sergent.m
+++ scripts/34x25_gauss/sergent_1982/uber_sergent_sergent.m
@@ -1,9 +1,8 @@
 clear all variables; clear all globals;
 
-plts = {'hu-encodings', 'hu-output','ls-bars','images','ffts','connectivity'};
-stats = {'hu-encodings', 'hu-output','images','ffts','connectivity'};
-sigma = [1.5 3 5 7 10 15];
-[args,opts]  = sergent_args('plots',plts,'stats',stats,'sigma',sigma,'runs',5);
+plts = {'ls-bars'};
+stats = {'images'};
+[args,opts]  = sergent_args('plots',plts,'stats',stats,'runs',5, 'ac.zscore', 0.025, 'ac.EtaInit', 5E-1, 'ac.AvgError', 5E-4  );
 
 % Run sergent task by training on all images
-[trn, tst] = de_SimulatorUber('uber/original', 'sergent_1982/de/sergent',         opts, args);
+[trn, tst] = de_SimulatorUber('uber/natimg', 'sergent_1982/de/sergent',         opts, args);
