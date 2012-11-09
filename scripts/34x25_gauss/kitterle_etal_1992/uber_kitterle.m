5dc93e0d821de0e1ab1b0181239822004e4e42ed
Ben Cipollini <bcipolli@ucsd.edu>
2012-11-09 06:24:35 -0800

    shifting to resilient backprop

--- scripts/34x25_gauss/kitterle_etal_1992/uber_kitterle.m
+++ scripts/34x25_gauss/kitterle_etal_1992/uber_kitterle.m
@@ -4,10 +4,19 @@ clear all variables; clear all globals;
 % Run the autoencoder & gather output
 %%%%%%%%%%%%%%%%%
 
-[args,opts] = kitterle_args( 'plots', {'hu-encodings', 'hu-output'}, 'stats',  {'hu-encodings', 'hu-output'} );
-
-[~, tst_freq] = de_SimulatorUber('uber/original', 'kitterle_etal_1992/sf_mixed/recog_freq', opts, args);
-[~, tst_type] = de_SimulatorUber('uber/original', 'kitterle_etal_1992/sf_mixed/recog_type', opts, args);
+[args,opts] = kitterle_args( 'runs', 5, 'plots', {'images', 'ffts'}, 'stats',  {'images','ffts'}, 'ac.zscore', 0.025, 'ac.EtaInit', 2E-1, 'ac.AvgError', 2E-4, 'ac.MaxIterations', 100,  ...
+'errorType', 1, ...
+                           'p.errorType', 3,... % cross-entropy
+                           'p.XferFn', [6 3], ...  %sigmoid->sigmoid
+                           'p.zscore', 0.15, ...
+                           'p.EtaInit', 1E-3, ...
+                           'p.dropout', 0, ...
+                           'p.nHidden', 250, ...
+                           'p.wmax', 2.0, ...
+                           'p.MaxIterations', 500);
+                           
+[~, tst_freq] = de_SimulatorUber('uber/natimg', 'kitterle_etal_1992/sf_mixed/recog_freq', opts, args);
+[~, tst_type] = de_SimulatorUber('uber/natimg', 'kitterle_etal_1992/sf_mixed/recog_type', opts, args);
 
 % Reconstitute into expected format
 ms.freq = tst_freq.models;
