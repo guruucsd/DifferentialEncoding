d37ccc22390ef417fe819e99fb2cb99f6a7b6570
Ben Cipollini <bcipolli@ucsd.edu>
2012-11-09 07:01:33 -0800

    enabling 68x50 with latest code

--- scripts/68x50_gauss/sergent_1982/uber_sergent_sergent.m
+++ scripts/68x50_gauss/sergent_1982/uber_sergent_sergent.m
@@ -1,7 +1,8 @@
 clear all variables; clear all globals;
 
-[args,opts]  = sergent_args();%'stats',{}, 'plots',{});
+plts = {'ls-bars'};
+stats = {'images'};
+[args,opts]  = sergent_args('plots',plts,'stats',stats,'runs',5, 'ac.zscore', 0.025, 'ac.EtaInit', 5E-1, 'ac.AvgError', 5E-4  );
 
 % Run sergent task by training on all images
-[trn, tst] = de_SimulatorUber('uber/original', 'sergent_1982/de/sergent', opts, args);
-
+[trn, tst] = de_SimulatorUber('uber/natimg', 'sergent_1982/de/sergent',         opts, args);
