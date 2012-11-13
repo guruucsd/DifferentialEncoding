1507a860bf4b7307f62305eb233442170b02005c
Ben Cipollini <bcipolli@ucsd.edu>
2012-11-13 11:30:00 +0900

    need to process each dataset separately

--- code/analyze/ac/plots/de_PlotDistributions.m
+++ code/analyze/ac/plots/de_PlotDistributions.m
@@ -23,29 +23,45 @@ function figs = de_PlotDistributions(mSets, stats)
 function fig = de_PlotDistributions1D(mSets, data, bins, figname)
 % Assume center of 2D data as center; all distances computed from there
 
-  [freq1D,rho] = guru_pixeldist(size(data));
+  fig = de_NewFig(figname);
+
+  [freq1D,rho] = guru_pixeldist(size(data{1}));
   nBins = length(bins); nFreq = length(freq1D);
 
   % Collect the samples & create each row of the image
-  fsamps      = cell(nFreq,1);
-  distn_surf = zeros(nFreq, nBins);
-  for fi=1:nFreq
-    fsamps{fi} = data(rho==freq1D(fi));
-    distn_surf(fi,:) = histc(fsamps{fi}, bins)/length(fsamps{fi});
+  distn_surf = zeros(2, nFreq, nBins);
+  for di=[1 length(data)]
+    fsamps      = cell(nFreq,1);
+    for fi=1:nFreq
+      fsamps{fi} = data{di}(rho==freq1D(fi));
+      distn_surf(di, fi, :) = histc(fsamps{fi}, bins)/length(fsamps{fi});
+    end;
+
+    % Show the surface
+    subplot(1,3,di);
+    surf(repmat(freq1D', [1 nBins]), repmat(bins, [nFreq 1]), squeeze(distn_surf(di,:,:)));
   end;
-
-  % Show the surface
-  surf(repmat(freq1D', [1 nBins]), repmat(bins, [nFreq 1]), distn_surf);
-
+  
+  subplot(1,3,3);
+  surf(repmat(freq1D', [1 nBins]), repmat(bins, [nFreq 1]), -squeeze(diff(distn_surf,1)));
+  
+  
 
 function fig = de_PlotMeanDistributions1D(mSets, data, bins, figname)
-  data_hist  = histc(data(:), bins)/numel(data);
-
+%
   fig = de_NewFig(figname);
-  bar(bins, data_hist);
+
+  data_hist1  = histc(data(:), bins)/numel(data{1});
+  data_hist2  = histc(data(:), bins)/numel(data{end});
+  data_hist_diff = data_hist1-data_hist2;
+  
+  subplot(1,3,1);bar(bins, data_hist1);
+  subplot(1,3,2);bar(bins, data_hist2);
+  subplot(1,3,3);bar(bins, data_hist_diff);
 
 
 function fig = de_PlotMeanDistributions2D(mSets, data, figname)
+%
 
   fig = de_NewFig(figname);
   max1 = max(data{1}(:));
@@ -56,7 +72,10 @@ function fig = de_PlotMeanDistributions2D(mSets, data, figname)
   d1 = data{1};
   d2 = data{2};
 
-  % 
+  % When the distribution is norme2,
+  %   there is a single location with an extreme value;
+  %   make sure when we get the clim, we take care
+  %   to exclude those extreme values
   if nnz(data{1}>=max1/2)==1 ...
     && nnz(data{2}>=max2/2)==1
 
