bb445b835e55e527bc1f6dfcc991644584d533a3
Ben Cipollini <bcipolli@ucsd.edu>
2012-11-16 02:56:27 -0800

    make autoencoder go faster. reduces asymmetry, but can get more done. may want to flip back.

--- scripts/pruning/autoencoder.m
+++ scripts/pruning/autoencoder.m
@@ -79,8 +79,8 @@ function [model,ws,s,fs] = autoencoder(model, ws)
       model.useBias = 1;
     case 'resilient'
       model.Pow     = 1;              % gradient power; Err = (y-y_hat).^(Pow+1)
-      model.EtaInit = 5E-3;          % Learning rate (to start)
-      model.Acc     = 5E-10;          % (1+Acc) Multiplicative increase to eta (when training good)
+      model.EtaInit = 2E-2;          % Learning rate (to start)
+      model.Acc     = 5E-5;          % (1+Acc) Multiplicative increase to eta (when training good)
       model.Dec     = 0.25;            % (1-Dec) Multiplicative decrease to eta (when training goes bad)
       model.XferFn  = [6 6];              % 1.73 * tanh
       model.useBias = 1;
