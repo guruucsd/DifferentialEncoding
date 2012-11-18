c9b4b2e13d45d012eac5f4cc614d88a811b9e938
Ben Cipollini <bcipolli@ucsd.edu>
2012-11-18 09:40:45 -0800

    reorganization; prunetrain should be in same codebase as other training

--- scripts/34x25_train/young_bion_1981/face_small.m
+++
@@ -1,7 +0,0 @@
-% Left-side-bias on small (34x25) images
-clear all variables; clear all globals;
-[args,opt] = face_args();
-
-% This will make the datasets
-fprintf('Making datasets...\n');
-[mSets, models, stats] = de_Simulator('young_bion_1981', 'orig', 'recog', {opt{:}}, args{:});
