4fdfc8f3eac1bfa66613a5cc99f2e7994b327277
Ben Cipollini <bcipolli@ucsd.edu>
2012-11-19 02:56:45 -0800

    creating initial christman script files; a few kitterle minor commenting changes

--- scripts/34x25/christman_etal_1991/pruning_christman.m
+++ scripts/34x25/christman_etal_1991/pruning_christman.m
@@ -4,6 +4,7 @@ stats =  {'hu-encodings', 'hu-output','ffts','images'};
 plots = stats;
 
 [args,opts] = uber_christman_args( 'runs', 25, 'plots', plots, 'stats', stats );
+[args]      = pruning_args( args{:} );
 
 [~, tst_low]   = de_SimulatorUber('uber/original', 'christman_etal_1991/low_freq/recog', opts, args);
 [~, tst_high]  = de_SimulatorUber('uber/original', 'christman_etal_1991/high_freq/recog', opts, args);
