b5936741a39b101df9dbcceff14d0ab73efd92d9
Ben Cipollini <bcipolli@ucsd.edu>
2012-11-19 00:57:10 -0800

    writing files for kitterle to work

--- scripts/34x25/kitterle_etal_1992/classic_kitterle_freq.m
+++ scripts/34x25/kitterle_etal_1992/classic_kitterle_freq.m
@@ -1,4 +1,4 @@
 % Kitterle (1992), classifying sin & square waves by frequency
-[args,opts] = kitterle_args( );
+[args,opts] = classic_kitterle_args( );
 
 [mSets, models, stats] = de_Simulator('kitterle_etal_1992', 'sf_mixed', 'recog_freq', {opts{:}}, args{:});
--- scripts/34x25/kitterle_etal_1992/pruning_kitterle.m
+++ scripts/34x25/kitterle_etal_1992/pruning_kitterle.m
@@ -5,9 +5,9 @@ stats={'images','connectivity','ffts'};
 plots=stats;
 
 [args,opts] = uber_kitterle_args( 'runs', 5, 'plots',plots,'stats',stats );
-                           
+[args]      = pruning_args( args{:} );
+
 [~, tst_freq] = de_SimulatorUber('uber/natimg', 'kitterle_etal_1992/sf_mixed/recog_freq', opts, args);
 [~, tst_type] = de_SimulatorUber('uber/natimg', 'kitterle_etal_1992/sf_mixed/recog_type', opts, args);
 
 kitterle_interaction_analysis(tst_freq, tst_type, ms, ss);
-
