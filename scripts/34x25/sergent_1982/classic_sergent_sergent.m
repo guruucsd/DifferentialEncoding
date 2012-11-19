5c0efe8f435bd9235dc89633e553ff02a0c6a712
Ben Cipollini <bcipolli@ucsd.edu>
2012-11-18 21:22:56 -0800

    reorganizing settings, to make more sense across simulation types

--- scripts/34x25/sergent_1982/classic_sergent_sergent.m
+++ scripts/34x25/sergent_1982/classic_sergent_sergent.m
@@ -1,5 +1,5 @@
 % Model run with targets and distracter sets swapped
 clear all variables;
 
-[args,opts] = sergent_args();
+[args,opts] = classic_sergent_args();
 [mSets, models, stats] = de_Simulator('sergent_1982', 'de', 'sergent', {opts{:}}, args{:});
