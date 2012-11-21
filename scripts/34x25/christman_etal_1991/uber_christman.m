f077bbe36524d6f3070e1707d510a6684a412dce
Ben Cipollini <bcipolli@ucsd.edu>
2012-11-20 22:52:23 -0800

    parameter changes

--- scripts/34x25/christman_etal_1991/uber_christman.m
+++ scripts/34x25/christman_etal_1991/uber_christman.m
@@ -3,9 +3,9 @@ clear all variables; clear all globals;
 stats =  {'ffts','images'};
 plots = stats;
 
-[args,opts] = uber_christman_args( 'runs', 25, 'plots', plots, 'stats', stats );
+[args,opts] = uber_christman_args( 'runs', 10, 'plots', plots, 'stats', stats );
 
-[~, tst_low]   = de_SimulatorUber('uber/original', 'christman_etal_1991/low_freq/recog', opts, args);
-[~, tst_high]  = de_SimulatorUber('uber/original', 'christman_etal_1991/high_freq/recog', opts, args);
+[~, tst_low]   = de_SimulatorUber('uber/natimg', 'christman_etal_1991/low_freq/recog', opts, args);
+[~, tst_high]  = de_SimulatorUber('uber/natimg', 'christman_etal_1991/high_freq/recog', opts, args);
 
 christman_interaction_analysis(tst_low, tst_high);
