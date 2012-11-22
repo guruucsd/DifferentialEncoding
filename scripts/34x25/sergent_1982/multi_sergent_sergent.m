fb39c9d6fc9f841044d38c74952fad60fdcb96d7
Ben Cipollini <bcipolli@ucsd.edu>
2012-11-21 19:29:38 -0800

    add support for MTL learning; clean up other de_DE code to follow cleaner logic of MTL

--- scripts/34x25/sergent_1982/multi_sergent_sergent.m
+++ scripts/34x25/sergent_1982/multi_sergent_sergent.m
@@ -4,7 +4,7 @@ clear all variables;
 stats = {};%{'images','ffts'};
 plts = {'ls-bars', stats{:}};
 
-[args,opts] = uber_sergent_args('plots',plts,'stats',stats,'runs',25, ...
-                           'deType', 'de', 'p.ndupes', 25, 'p.MaxIterations', 50, 'p.nHidden', 10, 'p.EtaInit', 5E-3, 'p.Acc', 1.001);
+[args,opts] = uber_sergent_args('plots',plts,'stats',stats,'runs',2, ...
+                           'deType', 'de-mtl','ac.AvgError', 5E-4, 'p.ndupes', 1, 'p.MaxIterations', 50, 'p.nHidden', 10, 'p.EtaInit', 5E-3, 'p.Acc', 1.001);
 % Run sergent task by training on all images
 [trn, tst] = de_SimulatorUber('uber/natimg', 'sergent_1982/de/sergent',         opts, args);
