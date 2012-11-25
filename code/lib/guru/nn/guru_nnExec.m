c6a45c8394fbf01a1cfc637d74c500ec9fc28088
Ben Cipollini <bcipolli@ucsd.edu>
2012-11-25 15:48:23 -0800

    fixing bug. No wonder we always saw the same thing--we were always grabbing the INPUT units from the output structuregit add ../../code/lib/guru/nn/guru_nnExec.m

--- code/lib/guru/nn/guru_nnExec.m
+++ code/lib/guru/nn/guru_nnExec.m
@@ -15,39 +15,46 @@ function [oact,err,huact] = guru_nnExec(model,X,Y)
   if (~isfield(model,'Conn')), model.Conn = double(model.Weights~=0); end;
   if isfield(model, 'linout'), warning('linout deprecated; use XferFn with multiple values instead.'); end;
 
+  % Calc
+  nUnits = size(model.Weights,1); nIn = size(X,1)-1; nOut = size(Y,1); nHid = nUnits-nIn-nOut-1; % remove input, output, and 
+  
   % Set up transfer function for each unit
-  nUnits = size(model.Weights,1); nOutput = size(Y,1); nInput = size(X,1); nHidden = nUnits-nOutput-nInput;
-  if (isfield(model, 'linout') && model.linout && length(model.XferFn) ~= (nHidden+prod(nOutput)))
-    model.XferFn = [model.XferFn*ones(1,nHidden) ones(1,nOutput)]; %linear hidden->output
+  if (isfield(model, 'linout') && model.linout && length(model.XferFn) ~= (nHid+prod(nOut)))
+    model.XferFn = [model.XferFn*ones(1,nHid) ones(1,nOut)]; %linear hidden->output
   elseif (length(model.XferFn)==2)
-    model.XferFn = [model.XferFn(1)*ones(1,nHidden) model.XferFn(2)*ones(1,nOutput)];
+    model.XferFn = [model.XferFn(1)*ones(1,nHid) model.XferFn(2)*ones(1,nOut)];
   end;
 
   % Special case for dropout
   %   reduce the value of the hidden->output weights by dropout %
   if (isfield(model, 'dropout'))
-    model.Weights(nInput+nHidden+[1:nOutput], nInput+[1:nHidden]) = (1-model.dropout)*model.Weights(nInput+nHidden+[1:nOutput], nInput+[1:nHidden]);
+    model.Weights(nIn+1+nHid+[1:nOut], nIn+1+[1:nHid]) = (1-model.dropout)*model.Weights(nIn+1+nHid+[1:nOut], nIn+1+[1:nHid]);
   end;
 
   % Execute the model and determine the errorType
   if ~isfield(model, 'ts')
-    [err,grad,o]=emo_backprop(X, Y, model.Weights, model.Conn, model.XferFn, model.errorType );
+    [err,~,o]=emo_backprop(X, Y, model.Weights, model.Conn, model.XferFn, model.errorType );
+  
+  % Multiple loops
   else
-    o = X(1:end-1,:);
+    if isfield(model, 'debug') && ismember(10,model.debug)
+      fprintf('Running %d re-entrant loops of model\n', model.ts);
+      keyboard
+    end;
+
+    o = X(1:nIn,:);
     for tsi=1:model.ts
-      if model.useBias
-        [err,grad,oz]=emo_backprop([o;X(end,:)], Y, model.Weights, model.Conn, model.XferFn, model.errorType );
-      else
-        [err,grad,oz]=emo_backprop(o, Y, model.Weights, model.Conn, model.XferFn, model.errorType );
-      end
-      o = oz(1:(size(X,1)-model.useBias),:);
-    end
+      %if model.useBias
+        [err,~,oz]=emo_backprop([o;X(end,:)], Y, model.Weights, model.Conn, model.XferFn, model.errorType );
+      %else
+      %  [err,grad,oz]=emo_backprop(o, Y, model.Weights, model.Conn, model.XferFn, model.errorType );
+      %end
+      o = oz(nIn+1+nHid+[1:nOut],:);
+    end;
   end;
 
-  nOutput = size(Y,1);
-  oact = o((end-nOutput+1):end,:);
+  oact = o(nIn+1+nHid+[1:nOut], :);
   
   if (nargout>2)
-    nInput = size(X,1);
-    huact = o((nInput+1):(end-nOutput),:);
+    huact = o(nIn+1+[1:nHid], :);
   end;
