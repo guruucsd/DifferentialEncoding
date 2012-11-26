f72414ea8236c17e2c2f6789c282fadd7dbf05c7
Ben Cipollini <bcipolli@ucsd.edu>
2012-11-26 01:48:45 -0800

    script for testing effect of img2pol

---
+++ scripts/34x25/testing/img2pol_sergent_sergent.m
@@ -0,0 +1,12 @@
+% Testing the effect of transforming the images to r/theta coordinates,
+%   a la Plaut & Behrmann 2011
+addpath('../sergent_1982');
+clear all variables; clear all globals;
+
+stats = {'paths','images','ffts'};%{'images','ffts'};
+plts = {'ls-bars', stats{:}};
+
+[args,opts]  = uber_sergent_args('plots',plts,'stats',stats,'runs',25);
+
+% Run sergent task by training on all images
+[trn, tst] = de_SimulatorUber('uber/natimg', 'sergent_1982/de/sergent', {opts{:}, 'img2pol'}, args);
