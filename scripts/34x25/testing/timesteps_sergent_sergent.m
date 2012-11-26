35510c773f3028d9dc07ea290621c8b9d7bbcbe7
Ben Cipollini <bcipolli@ucsd.edu>
2012-11-26 01:49:18 -0800

    param updates

--- scripts/34x25/testing/timesteps_sergent_sergent.m
+++ scripts/34x25/testing/timesteps_sergent_sergent.m
@@ -4,14 +4,14 @@ clear all variables;
 
 stats = {'ipd','images','ffts'};
 plts = {'ls-bars', stats{:}};
+[args,opts] = uber_sergent_args('plots',plts,'stats',stats,'runs',25,'ac.AvgError', 2E-4, 'ac.MaxIterations', 200, 'ac.Acc', 5E-5);
 
 % Run with ts=5
-[args5,opts5] = uber_sergent_args('plots',plts,'stats',stats,'runs',2,'ac.ts', 5);
-[trn5, tst5]  = de_SimulatorUber('uber/natimg', 'sergent_1982/de/sergent',         opts5, args5);
+[trn5, tst5]  = de_SimulatorUber('uber/natimg', 'sergent_1982/de/sergent', opts, {args{:}, 'ac.ts', 5});
 
 % Run with ts=1
 [args1,opts1] = uber_sergent_args('plots',plts,'stats',stats,'runs',2,'ac.ts', 1);
-[trn1, tst1]  = de_SimulatorUber('uber/natimg', 'sergent_1982/de/sergent',         opts1, args1);
+[trn1, tst1]  = de_SimulatorUber('uber/natimg', 'sergent_1982/de/sergent', opts, {args{:}, 'ac.ts', 1});
 
 % Compare!
-save('timesteps.mat','trn1','tst1','trn5','tst5');
+%save('timesteps.mat','trn1','tst1','trn5','tst5');
