c99f379114291e6567672722e0212c97297cf3db
Ben Cipollini <bcipolli@ucsd.edu>
2012-12-01 01:43:28 -0800

    parameter changes; things seem pretty good.  Migrating to img2pol to try christman and kitterle

--- scripts/34x25/sergent_1982/pruning_sergent_sergent.m
+++ scripts/34x25/sergent_1982/pruning_sergent_sergent.m
@@ -4,7 +4,7 @@ stats = {};%'connectivity', 'images','ffts'};
 plts = {'ls-bars', stats{:}};
 
 [args,opts] = uber_sergent_args('plots',plts,'stats',stats,'runs',25 );
-[args]      = pruning_args( args{:} );
+[args]      = pruning_args( args{:}, 'parallel', false );
 
 % Run sergent task by training on all images
-[trn, tst] = de_SimulatorUber('uber/natimg', 'sergent_1982/de/sergent', opts, args);
+[trn, tst] = de_SimulatorUber('vanhateren/natimg', 'sergent_1982/de/sergent', opts, args);
