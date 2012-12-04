d717c7471623c0303987d72498898df09fc4a0ef
Ben Cipollini <bcipolli@ucsd.edu>
2012-12-04 13:16:53 -0800

    getting pruning study to work

--- scripts/34x25/sergent_1982/pruning_sergent_sergent.m
+++ scripts/34x25/sergent_1982/pruning_sergent_sergent.m
@@ -3,8 +3,8 @@ clear all variables; clear all globals;
 stats = {'distns', 'images','ffts'};
 plts = {'ls-bars', stats{:}};
 
-[args,opts] = uber_sergent_args('plots',plts,'stats',stats,'runs',25, 'ac.EtaInit',1E-3,'sigma', [4 10]);
-[args]      = pruning_args( args{:} );
+args        = pruning_args('plots',plts,'stats',stats,'runs',5);
+[args,opts] = uber_sergent_args(args{:});
 
 % Run sergent task by training on all images
 [trn, tst] = de_SimulatorUber('vanhateren/100', 'sergent_1982/de/sergent', opts, args);
