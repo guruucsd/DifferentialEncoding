dd015012ddfcb387c2e50ccef165bf3a2df64cc2
Ben Cipollini <bcipolli@ucsd.edu>
2013-03-03 07:19:46 -0800

    updates to make it more flexible
    Former-commit-id: 69acbfea70a467f6ef81e067da1c2b1d3dafae77

--- runs/make_cache.m
+++ runs/make_cache.m
@@ -1,15 +1,26 @@
-addpath(genpath('../code'));
+function make_cache(dirs, cache_file);
 
-dirs = dir()
+  if ~exist('cache_file','var'), cache_file = 'cache_file.mat'; end;
+  if ~exist('get_cache_data','file'), addpath(genpath('../code')); end;
+  if ~exist('dirs','var')
+    dirs = dir();
+    dirs = {dirs([dirs.isdir]).name};
+    dirs = setdiff(dirs, {'.','..'});
+  elseif ischar(dirs)
+    dirs = {dirs};
+  elseif ~iscell(dirs)
+    error('dirs input var must be a string or a cell array of strings');
+  end;
 
-for di=1:length(dirs)
-    if ismember(dirs(di).name, {'.','..'}), continue; end; 
-    if ~dirs(di).isdir, continue; end;
-    fprintf('%s\n', dirs(di).name); 
-    get_cache_data(dirs(di).name, true); 
-end;
- 
-global g_data_cache g_dir_cache;
+  % Clear the global cache
+  global g_data_cache g_dir_cache;
+  g_data_cache = []; g_dir_cache = [];
 
-save('cache_file.mat', 'g_data_cache','g_dir_cache');
+  % Force loading of relevant cache data
+  for di=1:length(dirs)
+    fprintf('%s\n', dirs{di}); 
+    get_cache_data(dirs{di}, true); 
+  end;
+
+  save(cache_file, 'g_data_cache','g_dir_cache');
 
