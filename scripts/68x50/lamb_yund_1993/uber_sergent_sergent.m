5ea7a8b93e2c86cf7c2f7d8837a9878452f4e19c
Ben Cipollini <bcipolli@ucsd.edu>
2013-10-11 15:49:24 -0700

    tweaking params to make things run.

--- scripts/68x50/lamb_yund_1993/uber_sergent_sergent.m
+++ scripts/68x50/lamb_yund_1993/uber_sergent_sergent.m
@@ -6,4 +6,4 @@ plts = {'ls-bars', stats{:}};
 [args,opts]  = uber_sergent_args('plots',plts,'stats',stats,'runs',25);
 
 % Run sergent task by training on all images
-[trn, tst] = de_SimulatorUber('uber/natimg', 'sergent_1982/de/sergent',         opts, args);
+[trn, tst] = de_SimulatorUber('vanhateren/100', 'sergent_1982/de/sergent',         opts, args);
