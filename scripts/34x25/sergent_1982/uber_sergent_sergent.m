72952a2f265c0eca0430fed2caac2733f1fa0963
Ben Cipollini <bcipolli@ucsd.edu>
2014-02-04 15:38:26 -0800

    Moving parameters out of Sergent directory, to use across experiments (cross-entropy)

--- scripts/34x25/sergent_1982/uber_sergent_sergent.m
+++ scripts/34x25/sergent_1982/uber_sergent_sergent.m
@@ -1,9 +1,9 @@
 clear all variables; clear all globals;
 
-stats = {};%'ffts', 'images', 'connectivity'};
+stats = {'ffts', 'distns'};
 plts = {'ls-bars', stats{:}};
 
-[args,opts]  = uber_sergent_args('parallel', false, 'plots',plts, 'stats',stats, 'runs', 25);%, 'ac.AvgError', 0, 'ac.MaxIterations', 500);
+[args,opts]  = uber_sergent_args('parallel', false, 'plots',plts, 'stats',stats, 'runs', 68);%, 'ac.AvgError', 0, 'ac.MaxIterations', 500);
 
 % Run sergent task by training on all images
 [trn, tst] = de_SimulatorUber('vanhateren/250', 'sergent_1982/de/sergent', opts, args);
