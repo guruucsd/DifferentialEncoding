008aecc818f9af971d1fdcbf1b1ee161d77bf1c4
Ben Cipollini <bcipolli@ucsd.edu>
2014-05-27 20:11:19 -0700

    Add dataset for examining words.

---
+++ guru/img/guru_text2im.m
@@ -0,0 +1,13 @@
+function img = guru_text2im(str, width, height, varargin)
+
+    fh = figure('Visible', 'off'); 
+    imshow(0.5 * ones(height, width));
+
+    text(width/2, height/2, str, 'HorizontalAlignment', 'center', 'VerticalAlignment', 'middle', varargin{:});
+    set(gca, 'xlim', [0 width], 'ylim', [0 height]);
+
+    img = export_fig(fh);
+    yidx = [1:height] + round((size(img,1) - height + 0.5)/2);
+    xidx = [1:width] + round((size(img,2) - width + 0.5)/2);
+    img = double(img(yidx, xidx)) / 255;
+    close(fh);
