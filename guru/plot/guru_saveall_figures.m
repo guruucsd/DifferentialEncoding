9c208fb6e0b560d41ca025bdc3bd69162451bc17
Ben Cipollini <bcipolli@ucsd.edu>
2014-10-11 18:53:10 -0700

    More control over figure saving.
    Former-commit-id: 5ae83a2f93c61bf48d993bfbec3f5031ef71bc86

--- guru/plot/guru_saveall_figures.m
+++ guru/plot/guru_saveall_figures.m
@@ -1,14 +1,31 @@
-function guru_saveall_figures(base_name, type, overwrite)
+function guru_saveall_figures(base_name, types, overwrite, dirpath, close_after)
     if ~exist('base_name','var'), base_name='Fig'; end;
-    if ~exist('type', 'var'), type='png'; end;
-    if ~exist('overwrite','var'), overwrite=true; end;
+    if ~exist('types', 'var'), types={'png'}; end;
+    if ~iscell(types), types = {types}; end;
+    if ~exist('overwrite','var'), overwrite=false; end;
+    if ~exist('dirpath', 'var'), dirpath = pwd(); end;
+    if ~exist('close_after', 'var'), close_after = false; end;
 
     % print all figures
     fh = get(0,'Children');
+    suffs = zeros(size(types));
+
+    warning('off', 'MATLAB:prnRenderer:opengl');
     for fi=1:length(fh);
-    %    figure(fh);
-        fn = sprintf('%s%01d.%s', base_name, fh(fi), type);
-        if overwrite || ~exist(fn)
-            saveas(fh(fi), fn, type);
+        for ti=1:length(types)
+            while true
+                fn = fullfile(dirpath, sprintf('%s%01d.%s', base_name, fi+suffs(ti), types{ti}));
+                if exist(fn) && ~overwrite, suffs(ti) = suffs(ti) + 1;
+                else, break; end;
+            end;
+            if overwrite || ~exist(fn)
+                fprintf('Saving figure %d to %s.\n', fh(fi), fn);
+                switch types{ti}
+                    case 'fig', saveas(fh(fi), fn, types{ti});
+                    otherwise, export_fig(fh(fi), fn, '-painters');
+                end;
+                get(gcf, 'position')
+            end;
         end;
+        if close_after, close(fh(fi)); end;
     end;
