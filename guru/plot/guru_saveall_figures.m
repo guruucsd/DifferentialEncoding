95930bbfcc9c1df51acbad23ed0228b3ae1c1b89
Ben Cipollini <bcipolli@ucsd.edu>
2014-10-20 09:47:00 -0700

    tweaking code for reporting noise.
    Former-commit-id: 5454bb5399bf4c09bdf6451949132ec1d437d5fc

--- guru/plot/guru_saveall_figures.m
+++ guru/plot/guru_saveall_figures.m
@@ -1,9 +1,8 @@
-function guru_saveall_figures(base_name, types, overwrite, dirpath, close_after)
+function guru_saveall_figures(base_name, types, overwrite, close_after)
     if ~exist('base_name','var'), base_name='Fig'; end;
     if ~exist('types', 'var'), types={'png'}; end;
     if ~iscell(types), types = {types}; end;
     if ~exist('overwrite','var'), overwrite=false; end;
-    if ~exist('dirpath', 'var'), dirpath = pwd(); end;
     if ~exist('close_after', 'var'), close_after = false; end;
 
     % print all figures
@@ -14,17 +13,23 @@ function guru_saveall_figures(base_name, types, overwrite, dirpath, close_after)
     for fi=1:length(fh);
         for ti=1:length(types)
             while true
-                fn = fullfile(dirpath, sprintf('%s%01d.%s', base_name, fi+suffs(ti), types{ti}));
+                fn = sprintf('%s%01d.%s', base_name, fi+suffs(ti), types{ti});
                 if exist(fn) && ~overwrite, suffs(ti) = suffs(ti) + 1;
                 else, break; end;
             end;
             if overwrite || ~exist(fn)
                 fprintf('Saving figure %d to %s.\n', fh(fi), fn);
+                figure(fh(fi));
+                %set(fh(fi), 'PaperPositionMode', 'auto');
+                pos = get(fh(fi), 'Position');
+                set(fh(fi), 'PaperPosition', pos / 100);
+                get(fh(fi), 'position')
                 switch types{ti}
-                    case 'fig', saveas(fh(fi), fn, types{ti});
-                    otherwise, export_fig(fh(fi), fn, '-painters');
+                    case {'fig', 'png'}, saveas(fh(fi), fn, types{ti});
+                    otherwise, print(fh(fi), ['-d' types{ti}], fn); %export_fig(fn, '-painters');
                 end;
-                get(gcf, 'position')
+                get(fh(fi), 'position')
+                get(fh(fi), 'paperposition')
             end;
         end;
         if close_after, close(fh(fi)); end;
