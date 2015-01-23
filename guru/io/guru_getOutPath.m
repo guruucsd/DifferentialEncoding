ce49114c959755a8f1d06abaf0391cf2d9c78e57
Ben Cipollini <bcipolli@ucsd.edu>
2015-01-23 12:17:00 -0800

    Eliminate r_out_path (it was calling guru_getOutPath anyway); make output cache computation more simple.
    Former-commit-id: 4ebf6613dc079ebe8345c9aa6e37280eebfa994f

--- guru/io/guru_getOutPath.m
+++ guru/io/guru_getOutPath.m
@@ -9,24 +9,43 @@ function outdir = guru_getOutPath(dirtype)
 %
 
   switch (dirtype)
-    case {'cache'},
-        outdir = fullfile('~', '_cache');
+    case {'cache'}
+        project_name = guru_getProjectName();
+        outdir = fullfile('~', '_cache', project_name);
 
     case {'datasets'}
         outdir = fullfile('~', 'datasets');
 
-    case {'results'}
-        abc = dbstack;
-        script_name = abc(length(abc)).name;
-        script_dir = fileparts(which(script_name));
-        outdir = fullfile(script_dir, '..', '..', 'results');
+    case {'plot', 'results'}
+        project_dir = guru_getProjectDir();
+        outdir = fullfile(project_dir, 'results');
 
     otherwise
           error('Unknown type: %s', dirtype);
 end;
 
-%  if (~guru_findstr(de_GetBaseDir(), outdir) && ~guru_findstr('~', outdir))
-%    outdir = fullfile(de_GetBaseDir(), outdir);
-%  end;
+
+function script_name = guru_getScriptName()
+    abc = dbstack;
+    script_name = abc(length(abc)).name;
 
 
+function script_dir = guru_getScriptDir()
+    script_name = guru_getScriptName();
+    script_dir = fileparts(which(script_name));
+
+
+function project_dir = guru_getProjectDir()
+    d = guru_getScriptDir();
+    while d
+        if exist(fullfile(d, '.git'), 'dir')
+            break
+        end;
+        d = fileparts(d);
+    end;
+    project_dir = d;
+
+
+function project_name = guru_getProjectName()
+    project_dir = guru_getProjectDir();
+    project_name = guru_fileparts(project_dir, 'name');
