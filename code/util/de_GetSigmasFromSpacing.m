959ba09a4cc88f632675ff8911cc22e09aa9053e
Ben Cipollini <bcipolli@ucsd.edu>
2015-02-11 14:09:47 -0800

    Small bugfix, parameter changes, output silencing, etc.

--- code/util/de_GetSigmasFromSpacing.m
+++ code/util/de_GetSigmasFromSpacing.m
@@ -13,11 +13,11 @@ function sigma = de_GetSigmasFromSpacing(spacing)
 
     % Empirically, average spacing comparison of sigma(2) vs. sigma(1) is sqrt(sigma(2)/sigma(1))
     % for 2D sigma
-    dist_fn = @(sig) mean(sqrt(sum( mvnrnd(zeros(1, ndims(sig)), sig, 1000000).^2, 2)))
+    dist_fn = @(sig) mean(sqrt(sum( mvnrnd(zeros(1, ndims(sig)), sig, 1000000).^2, 2)));
     start_dist = 1.2918; %mean(sqrt(sum( mvnrnd([0 0], [1.5 0; 0 1/1.5], 1000000).^2, 2)))
-    sigma = (spacings / start_dist) .^2
+    sigma = (spacings / start_dist) .^2;
 
     % now test the result
     for si=1:length(sigma)
-        fprintf('Desired distance: %f; actual (estimated): %f\n', spacings(si), dist_fn(sigma(si)*[1.5 0; 0 1/1.5]));
+        fprintf('Desired distance: %f; actual (estimated): %.3f\n', spacings(si), dist_fn(sigma(si)*[1.5 0; 0 1/1.5]));
     end;
