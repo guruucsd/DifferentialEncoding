b5b9656cb48f0207a1dedc60d28447402f771b2a
Ben Cipollini <bcipolli@ucsd.edu>
2015-05-03 11:01:08 -0700

    Simplify Vishaal's contrast_balancing code

--- code/lib/guru/img/guru_contrast_balance_image.m
+++ code/lib/guru/img/guru_contrast_balance_image.m
@@ -1,101 +1,66 @@
-function [ return_image ] = guru_contrast_balance_image( image, border_width, fg_color, eight_neighbors )
+function [ return_image ] = guru_contrast_balance_image( image, border_width, new_bg_color, fg_color, eight_neighbors )
 %
 % Inputs:
 %   image           : 2D matrix of binary pixels
+%   border_width    : optional, # of pixels to fill from foreground.
+%   new_bg_color    : optional, background color after CB (default: image
+%   mean)
 %   fg_color        : optional, 0 or 1 (default: 1)
 %   eight_neighbors : optional, whether to use 4 or 8 neighbors (default: true)
-%   border_width    : optional, # of pixels to fill from foreground.
 %
 % Outputs:
 %   return_image    : contrast-balanced image, of same size as image.
 
-if ~exist('fg_color', 'var'), fg_color = 1; end;
-if ~exist('eight_neighbors', 'var'), eight_neighbors = true; end;
+all_colors = unique(image(:));
+guru_assert(length(all_colors) == 2, 'Input must be a binary image');
+
 if ~exist('border_width', 'var'), border_width = 1; end;
+if ~exist('new_bg_color', 'var'), new_bg_color = mean(image(:)); end;
+if ~exist('fg_color', 'var'), fg_color = max(image(:)); end;
+if ~exist('eight_neighbors', 'var'), eight_neighbors = true; end;
 
-bg_color = 1 - fg_color;
+bg_color = setdiff(all_colors, fg_color);
 [height, width] = size(image);
-total_color = 0;
 
-guru_assert(length(unique(image(:))) == 2, 'Input must be a binary image');
+return_image = new_bg_color * ones(size(image));
 
 for ii = 1:height
   for ij = 1:width
-    total_color = total_color + image(ii, ij);
     if image(ii, ij) == fg_color
+      return_image(ii, ij) = fg_color;
       continue;
     end
+    
     for ik = 1:border_width
-      if (ii-ik) > 0
-          if image(ii-ik, ij) == fg_color
-              image (ii, ij) = 0.5;
-              continue;
-          end
-      end
-      if (ii+ik) <= height
-          if image(ii + ik, ij) == fg_color
-              image (ii, ij) = 0.5;
-              continue;
-          end
-      end
-      if (ij-ik) > 0
-          if image(ii, ij-ik) == fg_color
-              image (ii, ij) = 0.5;
-              continue;
-          end
-      end
-      if (ij+ik) <= width
-          if image(ii, ij+ik) == fg_color
-              image(ii, ij) = 0.5;
-              continue;
-          end
-      end
-      if eight_neighbors == 1
-          for il = 1:border_width
-              if (ii-ik > 0) && (ij-il > 0)
-                  if image(ii-ik, ij-il) == fg_color
-                      image(ii, ij) = 0.5;
-                      continue;
-                  end
-              end
-              if (ii-ik > 0) && (ij+il <= width)
-                  if image(ii-ik, ij+il) == fg_color
-                      image(ii, ij) = 0.5;
-                      continue;
-                  end
-              end
-              if (ii+ik <= height) && (ij-il > 0)
-                  if image(ii+ik, ij-il) == fg_color
-                      image(ii, ij) = 0.5;
-                      continue;
-                  end
-              end
-              if (ii+ik <= height) && (ij+il <= width)
-                  if image(ii+ik, ij+il) == fg_color
-                      image(ii, ij) = 0.5;
-                      continue;
-                  end
-              end
-          end
+      if (ii-ik) > 0 && image(ii-ik, ij) == fg_color
+          return_image(ii, ij) = bg_color;
 
-      end
+      elseif (ii+ik) <= height && image(ii + ik, ij) == fg_color
+          return_image(ii, ij) = bg_color;
+          
+      elseif (ij-ik) > 0 && image(ii, ij-ik) == fg_color
+          return_image(ii, ij) = bg_color;
 
+      elseif (ij+ik) <= width && image(ii, ij+ik) == fg_color
+          return_image(ii, ij) = bg_color;
+
+      elseif eight_neighbors
+          for il=1:border_width
+              if (ii-ik > 0) && (ij-il > 0) && image(ii-ik, ij-il) == fg_color
+                  return_image(ii, ij) = bg_color;
+
+              elseif (ii-ik > 0) && (ij+il <= width) && image(ii-ik, ij+il) == fg_color
+                  return_image(ii, ij) = bg_color;
+
+              elseif (ii+ik <= height) && (ij-il > 0) && image(ii+ik, ij-il) == fg_color
+                  return_image(ii, ij) = bg_color;
+
+              elseif (ii+ik <= height) && (ij+il <= width) && image(ii+ik, ij+il) == fg_color
+                  return_image(ii, ij) = bg_color;
+
+              end
+          end;
+      end
     end
-
   end
-
-end
-
-average_color = total_color / (height * width);
-
-for ii=1:height
-    for ij=1:width
-        if image(ii, ij) == bg_color
-            image(ii, ij) = average_color;
-        elseif image(ii, ij) == 0.5
-            image(ii, ij) = bg_color;
-        end
-    end
-end
-return_image = image;
 end
