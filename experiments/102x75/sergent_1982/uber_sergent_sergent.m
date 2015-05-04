13812bb4c7b0b924321c50d7de437a23cde98afd
Ben Cipollini <bcipolli@ucsd.edu>
2015-05-04 05:37:41 -0700

    Tweaks to get this puppy running!

--- experiments/102x75/sergent_1982/uber_sergent_sergent.m
+++ experiments/102x75/sergent_1982/uber_sergent_sergent.m
@@ -6,4 +6,4 @@ plts = {'ls-bars', stats{:}};
 [args,opts]  = uber_sergent_args('parallel', false, 'plots',plts, 'stats',stats);
 
 % Run sergent task by training on all images
-[trn, tst] = de_SimulatorUber('vanhateren/250', 'sergent_1982/de/sergent', opts, args);
+[trn, tst] = de_SimulatorUber('vanhateren/100', 'sergent_1982/de/sergent', opts, args);
