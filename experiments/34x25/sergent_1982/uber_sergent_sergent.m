97266aa5c76dec8b303966f935802ffdf374156d
Ben Cipollini <bcipolli@ucsd.edu>
2015-05-10 22:41:44 -0700

    Tweaking args

--- experiments/34x25/sergent_1982/uber_sergent_sergent.m
+++ experiments/34x25/sergent_1982/uber_sergent_sergent.m
@@ -3,7 +3,7 @@ clear all variables; clear all globals;
 stats = {'ipd', 'ffts', 'distns', 'pca', 'images'};
 plts = {'ls-bars', stats{:}};
 
-[args,opts]  = uber_sergent_args('parallel', false, 'plots',plts, 'stats',stats);
+[args,opts]  = uber_sergent_args('parallel', false, 'plots',plts, 'stats',stats, 'runs', 50);
 
 % Run sergent task by training on all images
 [trn, tst] = de_SimulatorUber('vanhateren/250', 'sergent_1982/de/sergent', opts, args);
--- experiments/68x50/sergent_1982/uber_sergent_sergent.m
+++ experiments/68x50/sergent_1982/uber_sergent_sergent.m
@@ -1,9 +1,9 @@
 clear all variables; clear all globals;
 
-stats = {'ffts', 'images','ffts', 'distns', 'ipd'};
-plts = {'ls-bars', stats{:}};
+stats = {'ffts'};%, 'images','ffts', 'distns', 'ipd'};
+plts = {stats{:}};
 
-[args,opts]  = uber_sergent_args('plots', plts,'stats',stats, 'runs', 25);
+[args,opts]  = uber_sergent_args('plots', plts,'stats',stats, 'runs', 5);
 
 % Run sergent task by training on all images
 [trn, tst] = de_SimulatorUber('vanhateren/250', 'sergent_1982/de/sergent',         opts, args);
