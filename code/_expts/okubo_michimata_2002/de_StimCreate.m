9c8e699f5d2526f914563a7a8ea59592bfccd717
bcipolli <bcipolli@ucsd.edu>
2015-05-28 16:22:46 -0700

    Add de_StimCreate

--- code/_expts/okubo_michimata_2002/de_StimCreate.m
+++ code/_expts/okubo_michimata_2002/de_StimCreate.m
@@ -1,6 +1,64 @@
-%% Takes two parameters: height (vertical displacement of 2 dots from middle 5), above (1 for true, 0 for false, i.e. below)
+function [train,test,aux] = de_StimCreate(stimSet, taskType, opt)
+%
+%Input:
+%  stimSet  : a string specifying the (INPUT) image dataset. 
+%               'dots' => the "dot" stimuli shown on the left of Figure 1.
+%               'dots-cb' => the contrast balanced "dot" stimuli shown on the right of Figure 1. 
+%
+%  taskType : a string specifying the (OUTPUT) task.
+%               'categorical' => whether the dots are above or below the line.
+%               'coordinate' => how far the dots are from the line.
+% 
+%  opt      : a vector of options; all listed will be applied
+%               (none used here)
+%%
+%OUTPUT: three variables, two with the same structure.
+%
+% train, test: training and test data sets, containing the following fields:
+%    train.X    : matrix containing 16 vectors, each a unique hierarchical stimulus.
+%    train.TT   : 16 indices, one for each hierarchical stimulus, taken from TALL
+%    train.ST   : vector of 16 strings, describing each unique hierarchical stimulus.
+%               Tells the two stimuli and relationship that make up the hierarchical stimulus
+%    train.TIDX : ?
+%
+%    train.Y    : target vectors for the autoencoder (same as X)
+%    train.T    : target vectors for the perceptron (labels, based on task)
+%
+%    test.*     : same as train object, but
+%
+% aux: generic structure for saving off experiment-specific data. Here, contains:
+%    aux.idx : indexing of which trials correspond to local/global targets and distractors.
+%    aux.TLBL : human-readable labels for each of the perceptron outputs.
+
+  if (~exist('opt','var')),      opt      = {};     end;
+  if (~iscell(opt)),             opt      = {opt};  end;
+
+  % Create the input images.
+  heights = guru_getopt('heights', [[0.5 1.0 1.5 2.5 3.0 3.5] -1*[0.5 1.0 1.5 2.5 3.0 3.5]]);
+  train.nInput = [68 50];
+  train.X = zeros(prod(train.nInput), 0);
+  for h=heights:
+    train.X(:, end+1) = create_cat_coord_stimuli(h);
+  end;
+
+  % Create the output vectors.
+  if ~isempty(taskType)
+    switch (taskType)
+      case 'categorical', train.T = heights > 0;
+      case 'coordinate', train.T = abs(heights);
+      otherwise, error('Unknown taskType: %s', taskType);
+    end;
+  end;
+
+  % Now say that test data is the same as training data.
+  test     = train;
+  aux.heights = heights;
+
 
 function [ image ] = create_cat_coord_stimuli( height, above )
+% Takes two parameters:
+%   height (vertical displacement of 2 dots from middle 5), 
+%   above (1 for true, 0 for false, i.e. below)
 img_height = 50;
 img_width = 68;
 midline = img_height/2;
@@ -32,5 +90,5 @@ image(midline+height, 39) = 0;
 image(midline+height, 38) = 0;
 image(midline+height+1, 39) = 0;
 image(midline+height+1, 38) = 0;
-end
+
 
