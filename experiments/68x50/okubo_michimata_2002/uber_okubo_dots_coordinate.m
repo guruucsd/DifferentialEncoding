2f6f9a75855db49d67a282696e7aa67b063d3083
Ben Cipollini <bcipolli@ucsd.edu>
2015-05-28 16:34:45 -0700

    Adding experiment file 2

---
+++ experiments/68x50/okubo_michimata_2002/uber_okubo_dots_coordinate.m
@@ -0,0 +1,9 @@
+clear all variables; clear all globals;
+
+stats = {'images','ffts', 'distns', 'ipd'};
+plts = {stats{:}};
+
+[args,opts]  = uber_okubo_args('plots', plts,'stats',stats, 'runs', 2, 'ac.AvgError', 1E-3);
+
+% Run sergent task by training on all images
+[trn, tst] = de_SimulatorUber('vanhateren/250', 'okubo_michimata_2002/dots/coordinate', opts, args);
