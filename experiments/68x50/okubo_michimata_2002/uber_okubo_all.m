10bbae75d1313becb3dbeaa05b8002ae7e18d4b5
bcipolli <bcipolli@ucsd.edu>
2015-05-29 08:37:45 -0700

    Implement Okubo cross-experiment analysis & figure.

--- experiments/68x50/okubo_michimata_2002/uber_okubo_all.m
+++ experiments/68x50/okubo_michimata_2002/uber_okubo_all.m
@@ -10,3 +10,11 @@ plts = {stats{:}};
 [trn2, tst2] = de_SimulatorUber('vanhateren/250', 'okubo_michimata_2002/dots/coordinate', opts, args);
 [trn3, tst3] = de_SimulatorUber('vanhateren/250', 'okubo_michimata_2002/dots-cb/categorical', opts, args);
 [trn4, tst4] = de_SimulatorUber('vanhateren/250', 'okubo_michimata_2002/dots-cb/coordinate', opts, args);
+
+all_models = {tst1.models tst2.models tst3.models tst4.models};
+all_mSets = {tst1.mSets tst2.mSets tst3.mSets tst4.mSets};
+
+ss.group = de_StatsGroupBasicsOkubo(all_models, all_mSets);
+figs = de_PlotsGroupBasicsOkubo(all_mSets, ss.group);
+
+de_SavePlots(all_mSets{1}, figs);
