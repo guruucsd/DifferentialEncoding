2f702d041b0efcc4418ff715065ddef1502cdbc5
turtlebot <turtlebot@turtlebot>
2015-10-23 12:55:06 -0700

    Skeleton Code for Analysis and Plotting

--- code/_expts/slotnick_etal_2001/de_Analyzer.m
+++ code/_expts/slotnick_etal_2001/de_Analyzer.m
@@ -1,5 +1,5 @@
-function [stats, figs]  = de_Analyzer(mSets, mss)
-%function [stats, figs]  = de_Analyzer(mSets, mss)
+function [stats, figs]  = de_AnalyzerSF(mSets, mss)
+%function [stats, figs]  = de_AnalyzerSF(mSets, mss)
 %
 % Trains a differential encoder under the model and training parameters specified
 %
@@ -11,3 +11,11 @@ function [stats, figs]  = de_Analyzer(mSets, mss)
 % figs       :
 
   [stats,figs] = de_AnalyzerDE(mSets, mss);
+%  [stats.raw.r] = de_FindRejectionsRF(mss, mSets.rej, stats.raw, stats.raw.r);
+  [mss]        = de_DoRejections(mss, stats.raw.r);
+
+  % Do SF-specific stats & figs
+  [stats.rej.sf] = de_StaticizerCC(mSets, mss, stats);
+  [figs]         = [figs de_FigurizerCC(mSets, mss, stats) ];
+
+
--- experiments/68x50/slotnick_etal_2001/uber_slotnick_de.m
+++ experiments/68x50/slotnick_etal_2001/uber_slotnick_de.m
@@ -1,9 +1,9 @@
 clear all variables; clear all globals;
 
-stats = {'images'};%'images','ffts', 'distns', 'ipd'};
+stats = {'catcoord'};%'images','ffts', 'distns', 'ipd'};
 plts = {stats{:}};
 
-[args,opts]  = uber_slotnick_args('plots', plts,'stats',stats, 'runs', 25, 'ac.AvgError', 2E-4);
+[args,opts]  = uber_slotnick_args('plots', plts,'stats',stats, 'runs', 5, 'ac.AvgError', 2E-4);
 
 % Run sergent task by training on all images
 [trn, tst] = de_SimulatorUber('vanhateren/250', 'slotnick_etal_2001/de/categorical', opts, args)
