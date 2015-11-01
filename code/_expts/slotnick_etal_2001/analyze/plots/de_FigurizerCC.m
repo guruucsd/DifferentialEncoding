39ba213c32e0fb597a2d40c34fd49929da7891a6
vishaalprasad <vishaal.prasad@gmail.com>
2015-11-01 15:44:17 -0800

    Delegate to createfigure.m

--- code/_expts/slotnick_etal_2001/analyze/plots/de_FigurizerCC.m
+++ code/_expts/slotnick_etal_2001/analyze/plots/de_FigurizerCC.m
@@ -1,8 +1,6 @@
 %% Still needed:
 % 1) Make the plots prettier.
-% 2) Superimpose an error plot, or use mfe_barweb
-% 3) Use a better method than try/catch to determine if cat vs coord
-% 4) Implement code that generalizes based on the task (blob/dot vs paired
+% 2) Implement code that generalizes based on the task (blob/dot vs paired
 % squares etc)
 
 
@@ -10,42 +8,35 @@ function figs = de_FigurizerCC(mSets, mss, stats)
 
 ds = 'test';
 
-left = stats.rej.sf.perf.(ds){1};
-right = stats.rej.sf.perf.(ds){2};
+left = stats.rej.cc.perf.(ds){1};
+right = stats.rej.cc.perf.(ds){2};
 
-try % is this categorical? 
+if strcmp(mSets.data.taskType, 'categorical') 
     
     left_results = left{1};
     right_results = right{1};
     
-    left_average = mean(mean(left_results));
-    right_average = mean(mean(right_results));
+    left_average = mean(left_results(:)');
+    right_average = mean(right_results(:)');
     
     left_stddev = std(left_results(:)');
     right_stddev = std(right_results(:)');
-    fig = figure;
-    bar([left_average right_average], 0.4);
-    set(gca,'XTickLabel', {'LH', 'RH'});
-    ylim([0 0.5]);
-    title('Categorical Stimuli: Blob Dot'); 
-    
-   
+    fig = createfigure([left_average right_average], [left_stddev right_stddev], 'Categorical');
     figs = [fig];
-catch % must be coordinate
+
+elseif strcmp(mSets.data.taskType, 'coordinate') 
     
     left_results = left{2};
     right_results = right{2};
     
-    left_average = mean(mean(left_results));
-    right_average = mean(mean(right_results));
+    left_average = mean(left_results(:)');
+    right_average = mean(right_results(:)');
     
     left_stddev = std(left_results(:)');
     right_stddev = std(right_results(:)');
-    
-    fig = figure;
-    bar([left_average right_average], 0.4);
-    set(gca,'XTickLabel', {'LH', 'RH'});
-    ylim([0 0.5]);
-    title('Coordinate Stimuli: Blob Dot');
+    fig = createfigure([left_average right_average], [left_stddev right_stddev], 'Coordinate');
     figs = [fig];
+
+else
+    error('taskType is neither categorical nor coordinate');
 end
