388ccd9819ce385db44e9df51dd1c5fdf20218f7
Ben Cipollini <bcipolli@ucsd.edu>
2015-11-02 06:23:02 -0800

    stimSet: de => blob-dot. Add separate scripts for categorical, coordinate.

--- experiments/68x50/slotnick_etal_2001/uber_slotnick_blobdot_coordinate.m
+++ experiments/68x50/slotnick_etal_2001/uber_slotnick_blobdot_coordinate.m
@@ -6,4 +6,4 @@ plts = {stats{:}};
 [args,opts]  = uber_slotnick_args('plots', plts,'stats',stats, 'runs', 5, 'ac.AvgError', 2E-4);
 
 % Run sergent task by training on all images
-[trn, tst] = de_SimulatorUber('vanhateren/250', 'slotnick_etal_2001/de/categorical', opts, args)
+[trn, tst] = de_SimulatorUber('vanhateren/250', 'slotnick_etal_2001/blob-dot/coordinate', opts, args)
