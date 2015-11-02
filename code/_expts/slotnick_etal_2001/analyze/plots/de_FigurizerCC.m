1313c1496fc44f69c5533b0759084a9f55bd7764
Ben Cipollini <bcipolli@ucsd.edu>
2015-11-02 12:58:47 -0800

    Share plotting code across conditions; pass in # of runs. Change std computation.

--- code/_expts/slotnick_etal_2001/analyze/plots/de_FigurizerCC.m
+++ code/_expts/slotnick_etal_2001/analyze/plots/de_FigurizerCC.m
@@ -8,33 +8,26 @@ ds = 'test';
 
 left = stats.rej.cc.perf.(ds){1};
 right = stats.rej.cc.perf.(ds){2};
-
 if strcmp(mSets.data.taskType, 'categorical') 
-    
     left_results = left{1};
     right_results = right{1};
-    
-    left_average = mean(left_results(:)');
-    right_average = mean(right_results(:)');
-    
-    left_stddev = std(left_results(:)');
-    right_stddev = std(right_results(:)');
-    fig = de_CreateSlotnickFigure1([left_average right_average], [left_stddev right_stddev], 'Categorical');
-    figs = [fig];
 
 elseif strcmp(mSets.data.taskType, 'coordinate') 
-    
     left_results = left{2};
     right_results = right{2};
-    
-    left_average = mean(left_results(:)');
-    right_average = mean(right_results(:)');
-    
-    left_stddev = std(left_results(:)');
-    right_stddev = std(right_results(:)');
-    fig = de_CreateSlotnickFigure1([left_average right_average], [left_stddev right_stddev], 'Coordinate');
-    figs = [fig];
 
 else
     error('taskType is neither categorical nor coordinate');
 end
+
+
+rons = size(left_results, 1);
+
+left_average = mean(mean(left_results, 2));
+right_average = mean(mean(right_results, 2));
+
+left_stddev = std(mean(left_results, 2));
+right_stddev = std(mean(right_results, 2));
+
+fig = de_CreateSlotnickFigure1([left_average right_average], [left_stddev right_stddev], mSets.data.taskType, rons);
+figs = [fig];
