6196beb67107b03cab0d5dfc295f91b922bc1639
Vishaal Prasad <Vishaal@Vishaals-MacBook-Pro.local>
2015-11-13 01:31:33 -0800

    Changed Figurizer to work with new stats

--- code/_expts/slotnick_etal_2001/analyze/plots/de_FigurizerCC.m
+++ code/_expts/slotnick_etal_2001/analyze/plots/de_FigurizerCC.m
@@ -5,36 +5,28 @@ function figs = de_FigurizerCC(mSets, mss, stats)
 % squares etc)
 
 ds = 'test';
+taskType = strcat(upper(mSets.data.taskType(1)), mSets.data.taskType(2:end));
+
 
 left = stats.rej.cc.perf.(ds){1};
 right = stats.rej.cc.perf.(ds){2};
 
-if strcmp(mSets.data.taskType, 'categorical') 
-    
-    left_results = left{1};
-    right_results = right{1};
-    
-    left_average = mean(left_results(:)');
-    right_average = mean(right_results(:)');
-    
-    left_stddev = std(left_results(:)');
-    right_stddev = std(right_results(:)');
-    fig = de_CreateSlotnickFigure1([left_average right_average], [left_stddev right_stddev], 'Categorical');
-    figs = [fig];
+left_results1 = left{1};
+right_results1 = right{1};
 
-elseif strcmp(mSets.data.taskType, 'coordinate') 
-    
-    left_results = left{2};
-    right_results = right{2};
-    
-    left_average = mean(left_results(:)');
-    right_average = mean(right_results(:)');
-    
-    left_stddev = std(left_results(:)');
-    right_stddev = std(right_results(:)');
-    fig = de_CreateSlotnickFigure1([left_average right_average], [left_stddev right_stddev], 'Coordinate');
-    figs = [fig];
+left_results2 = left{2};
+right_results2 = right{2};
 
-else
-    error('taskType is neither categorical nor coordinate');
+left_results = [left_results1, left_results2];
+right_results = [right_results1, right_results2];
+
+left_average = mean(left_results(:)');
+right_average = mean(right_results(:)');
+
+left_stddev = std(mean(left_results, 2));
+right_stddev = std(mean(right_results, 2));
+fig = de_CreateSlotnickFigure1([left_average right_average], [left_stddev right_stddev], taskType);
+figs = [fig];
+
+    
 end
