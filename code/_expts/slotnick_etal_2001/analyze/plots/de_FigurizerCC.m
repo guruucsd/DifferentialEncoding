dfaf0eb77bae904e4ecad58ea962c141ce39dcff
Ben Cipollini <bcipolli@ucsd.edu>
2015-11-14 10:11:13 -0600

    Clean up plot, taskType=>taskTitle, specify # runs, use stderr.

--- code/_expts/slotnick_etal_2001/analyze/plots/de_FigurizerCC.m
+++ code/_expts/slotnick_etal_2001/analyze/plots/de_FigurizerCC.m
@@ -10,6 +10,7 @@ taskTitle = guru_capitalizeStr(mSets.data.taskType);
 
 left = stats.rej.cc.perf.(ds){1};
 right = stats.rej.cc.perf.(ds){end};
+rons = min(length(left), length(right));
 
 % these results are for on/near
 left_results1 = left{1};
@@ -26,8 +27,8 @@ right_results = [right_results1, right_results2];
 left_average = mean(left_results(:)');
 right_average = mean(right_results(:)');
 
-left_stddev = std(mean(left_results, 2));
-right_stddev = std(mean(right_results, 2));
+left_stderr = std(mean(left_results, 2)) / sqrt(size(left_results, 1));
+right_stderr = std(mean(right_results, 2)) / sqrt(size(right_results, 1));
 
-fig = de_CreateSlotnickFigure1([left_average right_average], [left_stddev right_stddev], taskTitle);
+fig = de_CreateSlotnickFigure1([left_average right_average], [left_stderr right_stderr], taskTitle, rons);
 figs = [fig];
