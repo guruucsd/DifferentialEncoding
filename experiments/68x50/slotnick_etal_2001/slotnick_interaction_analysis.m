2fdc655e0fa81b5299c778c87e94454f6413cf45
Vishaal Prasad <vishaal.prasad@gmail.com>
2015-11-21 14:51:33 -0800

    Return group plot and save

--- experiments/68x50/slotnick_etal_2001/slotnick_interaction_analysis.m
+++ experiments/68x50/slotnick_etal_2001/slotnick_interaction_analysis.m
@@ -10,5 +10,6 @@ function [ms,ss] = slotnick_interaction_analysis(tst_cate, tst_coor)
   mSets   = tst_cate.mSets;
 
   ss.group = de_StatsGroupBasicsSlot( mSets, ms, ss );
-  de_PlotGroupBasicsSlot(ms, ss);
+  [figs] = de_PlotGroupBasicsSlot(ms, ss);
+  de_SavePlots(mSets, figs);
 end
