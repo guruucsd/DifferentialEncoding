5417fea3fc2cb38673064acd671634c0c6e81663
Ben Cipollini <bcipolli@ucsd.edu>
2015-12-01 09:01:59 -0800

    BF: arg tweaks (remove img2pol/LVF) to get old results.

--- experiments/34x25/sergent_1982/uber_sergent_stacked.m
+++ experiments/34x25/sergent_1982/uber_sergent_stacked.m
@@ -1,14 +1,14 @@
 clear all variables; clear all globals;
 
 stats = {};%'ipd', 'ffts', 'distns', 'pca', 'images'};
-plts = {};%'ls-bars', stats{:}};
+plts = {'ls-bars'};%'ls-bars', stats{:}};
 
 [args,opts]  = uber_sergent_args('deType', 'de-stacked', 'p.lambda', 0.01, ...
                                  'plots', plts, 'stats', stats, 'runs', 2);
 
 % Make some things consistent across AC and P
-ac_xferfn = guru_getopt(args, 'ac.XferFn', [6 3]);
-args(end + [1:2]) = {'p.XferFn', [ac_xferfn(1) guru_getopt(args, 'p.XferFn', [6 1])]};
+ac_xferfn = guru_getopt(args, 'ac.XferFn', [6 1]);
+args(end + [1:2]) = {'p.XferFn', [ac_xferfn(1) guru_getopt(args, 'p.XferFn', [6 3])]};
 for prop = {'zscore', 'noise_input', 'wlim'}
     prop = prop{1};
     ac_val = guru_getopt(args, ['ac.' prop]);
