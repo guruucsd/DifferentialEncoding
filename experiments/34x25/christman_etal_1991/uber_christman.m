891bea15d326223d5961d663777e5e30b3b4fa1d
Ben Cipollini <bcipolli@ucsd.edu>
2015-12-02 06:46:27 -0800

    STY, BF, ENH: clean up, refactor, and reset argument files.

--- experiments/34x25/christman_etal_1991/uber_christman.m
+++ experiments/34x25/christman_etal_1991/uber_christman.m
@@ -1,6 +1,6 @@
 clear all variables; clear all globals;
 
-stats =  {'ffts','images'};
+stats =  {}; %'ffts','images'};
 plots = stats;
 
 [args,opts] = uber_christman_args( 'plots', plots, 'stats', stats);
