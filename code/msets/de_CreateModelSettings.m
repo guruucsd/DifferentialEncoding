9df2debc36f859a6d3b9d820347e1bf397f734f0
Ben Cipollini <bcipolli@ucsd.edu>
2015-12-02 14:59:49 -0800

    RF: call de_SetupStackedArgs automatically, so 'stacked' script files simply need deType='de-stacked'

--- code/msets/de_CreateModelSettings.m
+++ code/msets/de_CreateModelSettings.m
@@ -1,19 +1,26 @@
 function mSets = de_CreateModelSettings(varargin)
 
-    % set from input
+    % Set from input
     mSets = guru_stampProps(struct(), varargin{:});
-    nSigmas = length(mSets.sigma);
 
-    % Load settings from input file
+    if strcmp(mSets.deType, 'de-stacked')
+        if isfield(mSets, 'p')
+            mSets = de_SetupStackedArgs(mSets);
+        else
+            % ac-only; take advantage of any cached ac.
+            mSets.deType = 'de';
+        end;
+    end;
+
+
+    % Massage some values
     if (~isfield(mSets,      'data')),     mSets.data = load(mSets.dataFile); end;
-
     if (~isfield(mSets.data, 'taskType')), mSets.data.taskType = ''; end;
     if (~isfield(mSets.out, 'files')),     mSets.out.files       = {}; end;
 
     % Restamp some properties
     mSets.nInput  = mSets.data.train.nInput;
     mSets.nOutput = mSets.data.train.nInput;
-
 
     % Get the output directory name
     base_runspath = mSets.out.runspath;
@@ -22,11 +29,12 @@ function mSets = de_CreateModelSettings(varargin)
     mSets.out.dirstem = {};
     mSets.out.runspath = {};
 
+    % Massage arguments
     if length(mSets.nHidden) == 1, mSets.nHidden = repmat(mSets.nHidden, size(mSets.sigma)); end;
     if length(mSets.nConns)  == 1, mSets.nConns  = repmat(mSets.nConns, size(mSets.sigma)); end;
     if length(mSets.hpl)     == 1, mSets.hpl     = repmat(mSets.hpl, size(mSets.sigma)); end;
 
-    for si=1:nSigmas
+    for si=1:length(mSets.sigma)
         mSets.out.dirstem{si}  = de_GetDataFile( ...
             mSets.expt, ...
             mSets.data.stimSet, ...
@@ -41,26 +49,19 @@ function mSets = de_CreateModelSettings(varargin)
         mSets.out.runspath{si}    = fullfile(base_runspath, mSets.out.dirstem{si});
     end;
 
-        mSets.out.shared_dirstem = de_GetDataFile( ...
-            mSets.expt, ...
-            mSets.data.stimSet, ...
-            mSets.data.taskType, ...
-            mSets.data.opt, ...
-            'mixed', ...
-            'dir', ...
-            '' ...
-        );
+    mSets.out.shared_dirstem = de_GetDataFile( ...
+        mSets.expt, ...
+        mSets.data.stimSet, ...
+        mSets.data.taskType, ...
+        mSets.data.opt, ...
+        'mixed', ...
+        'dir', ...
+        '' ...
+    );
 
-        % Append
-        mSets.out.resultspath    = fullfile(base_resultspath, mSets.out.shared_dirstem);
+    % Append
+    mSets.out.resultspath    = fullfile(base_resultspath, mSets.out.shared_dirstem);
 
-    % Put results
-    %unique_dirstems = unique(mSets.out.dirstem);
-    %if length(unique_dirstems) == 1
-    %    mSets.out.resultspath = fullfile(base_resultspath, unique_dirstems{1});
-    %else
-    %    mSets.out.resultspath = fullfile(base_resultspath, 'mixed_network_comparison');
-    %end;
 
     %
     mSets.data.train = de_NormalizeDataset(mSets.data.train, mSets);
--- code/msets/de_SetupStackedArgs.m
+++ code/msets/de_SetupStackedArgs.m
@@ -1,13 +1,32 @@
 function [args, opts] = de_SetupStackedArgs(args, opts)
-	args(end+1:end+2) = {'deType', 'de-stacked'};
-                                 
-	% Grab the AC hidden unit XferFn
-	ac_xferfn = guru_getopt(args, 'ac.XferFn', [6 1]);
-	args(end + [1:2]) = {'p.XferFn', [ac_xferfn(1) guru_getopt(args, 'p.XferFn', [6 3])]};
+%function [args, opts] = de_SetupStackedArgs(args, opts)
+%
 
-	% Copy some properties from the autoencoder to the perceptron.
-	for prop = {'zscore', 'noise_input'}
-		prop = prop{1};
-		ac_val = guru_getopt(args, ['ac.' prop]);
-		args(end + [1:2]) = {['p.' prop], ac_val};
-	end;
+    if iscell(args)
+        args(end+1:end+2) = {'deType', 'de-stacked'};
+
+        % Grab the AC hidden unit XferFn
+        ac_xferfn = guru_getopt(args, 'ac.XferFn', [6 1]);
+        args(end + [1:2]) = {'p.XferFn', [ac_xferfn(1) guru_getopt(args, 'p.XferFn', [6 3])]};
+
+        % Copy some properties from the autoencoder to the perceptron.
+        for prop = {'zscore', 'noise_input'}
+            prop = prop{1};
+            ac_val = guru_getopt(args, ['ac.' prop]);
+            args(end + [1:2]) = {['p.' prop], ac_val};
+        end;
+
+    elseif isstruct(args)
+        mSets = args;
+        mSets.deType = 'de-stacked';
+        mSets.p.XferFn = [mSets.ac.XferFn(1), mSets.p.XferFn];
+        for prop = {'zscore', 'noise_input'}
+            prop = prop{1};
+            if isfield(mSets.ac, prop)
+                mSets.p.(prop) = mSets.ac.(prop);
+            end;
+        end;
+        args = mSets;
+    else
+        error('Unrecognized type for args');
+    end;
