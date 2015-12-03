02596001efa7bc16ea80d955338feecd08327e89
Ben Cipollini <bcipolli@ucsd.edu>
2015-12-03 05:52:26 -0800

    BF: we want to know combined data (fig2), as well as easy(fig3)/hard(fig4)

--- code/_expts/slotnick_etal_2001/analyze/plots/de_FigurizerCC.m
+++ code/_expts/slotnick_etal_2001/analyze/plots/de_FigurizerCC.m
@@ -13,22 +13,37 @@ function figs = de_FigurizerCC(mSets, mss, stats)
     figs = de_NewFig(mSets.data.taskType);
 
     n_trial_types = length(left);
-    for ti=1:n_trial_types  % plot for each trial type
-        subplot(1, n_trial_types, ti);
+    n_plots = n_trial_types + 1;  % plot for each trial type, plus combined
+    for pi=1:n_plots
+        subplot(1, n_tplots, pi);
 
-        left_mean = mean(left{ti}(:));
-        right_mean = mean(right{ti}(:));
+        if pi == 1
+            left_mean = mean(cellfun(@(d) mean(d(:)), left));
+            right_mean = mean(cellfun(@(d) mean(d(:)), right));
+            left_stderr = 0 * left_mean;  % feeling lazy; errorbars are clear
+            right_stderr = 0 * right_mean; % on other subplots.
+            taskTitle = guru_captializeStr(mSets.data.taskType);
 
-        % Average over trials to get a single score per network,
-        % then find stderr over all networks.
-        left_stderr = std(mean(left{ti}, 2)) / sqrt(size(left{ti}, 1));
-        right_stderr = std(mean(right{ti}, 2)) / sqrt(size(right{ti}, 1));
+        else
+            ti = pi - 1;
+            left_mean = mean(left{ti}(:));
+            right_mean = mean(right{ti}(:));
 
-        taskTitle = sprintf('%s (%s)', ...
-                            guru_capitalizeStr(mSets.data.taskType), ...
-                            guru_capitalizeStr(stats.rej.cc.anova.(ds).trial_types{ti}));
+            % Average over trials to get a single score per network,
+            % then find stderr over all networks.
+            left_stderr = std(mean(left{ti}, 2)) / sqrt(size(left{ti}, 1));
+            right_stderr = std(mean(right{ti}, 2)) / sqrt(size(right{ti}, 1));
+
+            taskTitle = sprintf('%s (%s)', ...
+                                guru_capitalizeStr(mSets.data.taskType), ...
+                                guru_capitalizeStr(stats.rej.cc.anova.(ds).trial_types{ti}));
+        end;
 
         de_CreateSlotnickFigure1([left_mean right_mean], ...
                                  [left_stderr right_stderr], ...
                                  taskTitle, mSets.data.stimSet, rons);
+
+        if pi ~= round(n_plots/2)
+            xlabel('');  % remove label
+        end;
     end;
