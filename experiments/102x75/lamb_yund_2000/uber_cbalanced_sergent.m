c242797a41fe5cb7b9d5de7374865a52c99d76e5
bcipolli <bcipolli@ucsd.edu>
2015-12-03 22:19:40 -0800

    Han et al can only be run in 102x75.

--- experiments/102x75/lamb_yund_2000/uber_cbalanced_sergent.m
+++
@@ -1,7 +0,0 @@
-stats = {'ipd', 'ffts', 'distns', 'pca', 'images'};
-plts = {'ls-bars', stats{:}};
-
-[args,opts]  = uber_cbalanced_args('plots', plts, 'stats', stats, 'runs', 10);
-
-% Run sergent task by training on all images
-[trn, tst] = de_SimulatorUber('vanhateren/250', 'lamb_yund_2000/contrast-balanced/de', opts, args);
