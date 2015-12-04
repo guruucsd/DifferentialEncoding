fc0006bce8377df954a0c1d83e2b5b726e04d700
Ben Cipollini <bcipolli@ucsd.edu>
2015-12-04 07:45:37 -0800

    BF: make okubo plot more appealing.

--- code/_expts/okubo_michimata_2002/analyze/plots/de_PlotsGroupBasicsOkubo.m
+++ code/_expts/okubo_michimata_2002/analyze/plots/de_PlotsGroupBasicsOkubo.m
@@ -14,13 +14,13 @@ for si=1:2
 
     % NOTE the use of MEDIAN error (as specified in the paper)
     subplot(1, 2, si);
-    errorbar([0 0; 1 1], stats.median_error(expt_idx, hemi_idx), stats.std_error(expt_idx, hemi_idx), 'LineWidth', 3.0);
+    errorbar([1 1; 2 2;], stats.median_error(expt_idx, hemi_idx), stats.std_error(expt_idx, hemi_idx), 'LineWidth', 3.0);
 
-    set(gca, 'xtick', 0:1, 'xticklabel', expt_names(expt_idx));
+    set(gca, 'xtick', 1:2, 'xticklabel', expt_names(expt_idx));
+    set(gca, 'xlim', [0.5, 2.5]);
+    set(gca, 'ylim', [min(stats.median_error(:) - stats.std_error(:)) ...
+                      max(stats.median_error(:) + stats.std_error(:))] ...;
+                     .* [0.9 1.1]);
     legend({'RH', 'LH'});
     title(stim_names(expt_idx(1)));
-
-    set(gca, 'ylim', [min(stats.median_error(:) - stats.std_error(:)) ...
-                      max(stats.median_error(:) - stats.std_error(:))] ...;
-                     .* [0.9 1.1]);
 end;
