7b7ea87cac8491825be5ae9d4325335e1395d797
bcipolli <bcipolli@ucsd.edu>
2015-12-03 20:05:44 -0800

    STY: suppress output in scripts, remove overridding parameters from testing.

--- experiments/68x50/okubo_michimata_2002/uber_okubo_dots_categorical.m
+++ experiments/68x50/okubo_michimata_2002/uber_okubo_dots_categorical.m
@@ -1,9 +1,4 @@
-clear all variables; clear all globals;
-
-stats = {'images'};%'images','ffts', 'distns', 'ipd'};
-plts = {stats{:}};
-
-[args,opts]  = uber_okubo_args('plots', plts,'stats',stats, 'runs', 25);
+[args,opts]  = uber_okubo_args();
 
 % Run sergent task by training on all images
 [trn, tst] = de_SimulatorUber('vanhateren/250', 'okubo_michimata_2002/dots/categorical', opts, args);
--- experiments/68x50/slotnick_etal_2001/uber_slotnick_blobdot_categorical.m
+++ experiments/68x50/slotnick_etal_2001/uber_slotnick_blobdot_categorical.m
@@ -1,4 +1,4 @@
 [args, opts]  = uber_slotnick_args();
 
 % Run sergent task by training on all images
-[trn, tst] = de_SimulatorUber('vanhateren/250', 'slotnick_etal_2001/blob-dot/categorical', opts, args)
+[trn, tst] = de_SimulatorUber('vanhateren/250', 'slotnick_etal_2001/blob-dot/categorical', opts, args);
