2cf69693879cf3bd91d0905935b0b025f0f3b1e5
bcipolli <bcipolli@ucsd.edu>
2015-12-03 23:10:09 -0800

    fix: slotnick stims

--- code/_expts/slotnick_etal_2001/de_StimCreate.m
+++ code/_expts/slotnick_etal_2001/de_StimCreate.m
@@ -153,8 +153,6 @@ function train = MakePlusMinus(taskType)
 function image = paired_squares_stimuli(left_distance, right_distance, four_pixel)
 % Takes three parameters: 2 are distances from quartile lines
 % Last parameter: if four_pixel = 1, then the square will be 4 pixels. For all other values it will be 1 pixel
-% If four_pixel = 1, valid range for distances is  [2, 4]. If four_pixel != 1, valid range is [2, 5]. Anything
-% outside this range will have weird output.
 
     image_height = 34;
     image_width = 25;
@@ -176,26 +174,6 @@ function image = paired_squares_stimuli(left_distance, right_distance, four_pixe
     image(horizontal_midline, left_quarterline+left_distance) = 0;
     image(horizontal_midline, right_quarterline+right_distance) = 0;
     image(horizontal_midline, right_quarterline-right_distance+1) = 0;
-
-    if (four_pixel == 1)
-
-        image(horizontal_midline, left_quarterline-left_distance) = 0;
-        image(horizontal_midline+1, left_quarterline-left_distance) = 0;
-        image(horizontal_midline+1, left_quarterline-left_distance+1) = 0;
-
-        image(horizontal_midline+1, left_quarterline+left_distance) = 0;
-        image(horizontal_midline, left_quarterline+left_distance+1) = 0;
-        image(horizontal_midline+1, left_quarterline+left_distance+1) = 0;
-
-
-        image(horizontal_midline, right_quarterline-right_distance) = 0;
-        image(horizontal_midline+1, right_quarterline-right_distance) = 0;
-        image(horizontal_midline+1, right_quarterline-right_distance+1) = 0;
-
-        image(horizontal_midline+1, right_quarterline+right_distance) = 0;
-        image(horizontal_midline, right_quarterline+right_distance+1) = 0;
-        image(horizontal_midline+1, right_quarterline+right_distance+1) = 0;
-    end
 
 
 function rot_image = blob_stimuli(distance, dot_size, orientation)
--- experiments/testing/test_datasets.m
+++ experiments/testing/test_datasets.m
@@ -7,8 +7,8 @@ addpath(genpath(code_dir));
 % de_MakeDataset('okubo_michimata_2002', 'dots-cb', 'categorical', {'medium'}, true, true);
 % de_MakeDataset('okubo_michimata_2002', 'dots', 'coordinate', {'medium'}, true, true);
 
- de_MakeDataset('slotnick_etal_2001', 'blob-dot', 'categorical', {'medium'}, true, true);
+% de_MakeDataset('slotnick_etal_2001', 'blob-dot', 'categorical', {'medium'}, true, true);
 % de_MakeDataset('slotnick_etal_2001', 'blob-dot', 'coordinate', {'medium'}, true, true);
-% de_MakeDataset('slotnick_etal_2001', 'paired-squares', 'coordinate', {'small'}, true, true);
+ de_MakeDataset('slotnick_etal_2001', 'paired-squares', 'coordinate', {'small'}, true, true);
 % de_MakeDataset('slotnick_etal_2001', 'plus-minus', 'categorical', {'small'}, true, true);
 % de_MakeDataset('slotnick_etal_2001', 'plus-minus', 'coordinate', {'small'}, true, true);
