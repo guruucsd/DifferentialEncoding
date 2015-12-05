71d3c9edca1e3245dcf73780251fa69b91c24549
bcipolli <bcipolli@ucsd.edu>
2015-12-05 13:17:09 -0800

    ENH: clean freq preferences, generalize to orientation & phase.

--- code/analyze/ac/plots/de_PlotFreqPreferences.m
+++ code/analyze/ac/plots/de_PlotFreqPreferences.m
@@ -1,53 +1,65 @@
 function figs = de_PlotFreqPreferences(mSets, stats)
 % Find the max index at each location.
 
-    freqs = stats.settings.cpi;
-    n_freqs = length(freqs);
     sigmas = mSets.sigma;
     n_sigmas = length(sigmas);
+    dset = stats.settings.dset;
+    figs = de_VisualizeDataset(dset);
 
-    norm_imgs = zeros(n_sigmas, mSets.nInput(1), mSets.nInput(2));
-    distn = zeros(n_sigmas, n_freqs);
+    for prop={'freq', 'orient', 'phase'}
+      prop = prop{1};
+      prop_vals = dset.([guru_iff(strcmp(prop, 'freq'), 'cycle', prop) 's']);
+      n_prop_vals = length(prop_vals);
 
-    figs = de_NewFig('dummy');
+      for val={'mean', 'std'}
+        val=val{1};
 
-    for si=1:n_sigmas
-      [~, max_resp_idx] = max(stats.avg_resp{si}, [], 3);
+        norm_imgs = zeros(n_sigmas, mSets.nInput(1), mSets.nInput(2));
+        distn = zeros(n_sigmas, n_prop_vals);
 
-      n_models = size(max_resp_idx, 1);
-      imgs = reshape(max_resp_idx, [n_models * mSets.hpl(si), mSets.nInput]);
+        for si=1:n_sigmas
+          [~, max_resp_idx] = max(stats.(prop).(val){si}, [], 3);
+          n_models = size(max_resp_idx, 1);
+          imgs = reshape(max_resp_idx, [n_models * mSets.hpl(si), mSets.nInput]);
 
-      % Plot 1: distribution of the average max, across the image.
-      norm_imgs(si, :, :) = mean(imgs);
+          % distribution of the average max, across the image.
+          norm_imgs(si, :, :) = mean(imgs, 1);
 
-      % Plot 2
-      distn(si, :) = histc(imgs(:), 1:n_freqs) / numel(imgs);
-    end;
+          % distribution of preferences
+          distn(si, :) = histc(imgs(:), 1:n_prop_vals) / numel(imgs);
+        end;
 
-    if true
-      figs = de_NewFig('freq-pref-as-image');
-      set(figs(end).handle, 'Position', [0, 0, 1200, 600]);
+        % Figure 1
+        if false
+        figs(end+1) = de_NewFig(sprintf('%s-%s-pref-as-image', val, prop));
 
-      for si=1:n_sigmas
-        subplot(1, n_sigmas + 1, si);
-        imagesc(squeeze(norm_imgs(si, :, :)), [1 n_freqs]);
-        colorbar;
-      end;
+        for si=1:n_sigmas
+          subplot(1, n_sigmas + 1, si);
+          imagesc(squeeze(norm_imgs(si, :, :)), [1 n_prop_vals]);
+        end;
 
-      subplot(1, n_sigmas + 1, n_sigmas + 1);
-      distn_diff = diff(norm_imgs([1 end], :, :), 1, 1);
-      imagesc(squeeze(distn_diff), (n_freqs - 1) * [-1 1]);
-      colorbar;
-      xlabel('RH preference - LH preference');
+        distn_diff = diff(norm_imgs([1 end], :, :), 1, 1);
+        subplot(1, n_sigmas + 1, n_sigmas + 1);
+        imagesc(squeeze(distn_diff), (n_prop_vals - 1) * [-1 1]);
+        xlabel(sprintf('RH - LH %s %s preference', val, prop));
 
-      colormap jet;
-    end;
+        colormap jet;
+        set(figs(end).handle, 'Position', [0, 0, 1200, 600]);
+        end;
 
+        % Figure 2
+        figs(end+1) = de_NewFig(sprintf('%s-%s-pref-distns', val, prop));
+        bar(distn');
+        legend(arrayfun(@(s) sprintf('%.2f', s), sigmas, 'UniformOutput', false));
+        xticks = get(gca, 'xtick');
+        hit_idx = 1 <= xticks & xticks <= n_prop_vals;
+        xticklabels = arrayfun(@(v) sprintf('%.2f', v), prop_vals(xticks(hit_idx)), ...
+                               'UniformOutput', false);
+        set(gca, 'xtick', xticks(hit_idx), 'xticklabel', xticklabels);
+        xlabel(prop);
+        ylabel('Proportion of units');
+        title(sprintf('%s %s', val, prop));
+        set(figs(end).handle, 'Position', [0, 0, 1200, 800]);
 
-    if true
-      figs(end+1) = de_NewFig('freq-pref-distns');
-      bar(distn');
-      set(gca, 'xticklabel', freqs(get(gca, 'xtick')));
-      xlabel('Frequency (cpi)');
-      ylabel('Proportion of units');
-    end;
+      end;  % val
+    end;  % prop
--- code/analyze/ac/stats/de_StatsFreqPreferences.m
+++ code/analyze/ac/stats/de_StatsFreqPreferences.m
@@ -2,146 +2,70 @@ function [stats] = de_StatsFreqPreferences(mss, varargin)
 %
 % Returns the distribution of weights and connections over all models within each sigma
 
+  % Retrieve settings
   mSets = mss{end}(end);
+  opt = {mSets.data.train.opt, varargin{:}};
+  n_phases = guru_getopt(opt, 'nphases', 8);
+  n_orients = guru_getopt(opt, 'nthetas', 8);
+  n_freqs = guru_getopt(opt, 'nfreqs', 8);
 
-  p = struct( 'a_mode', 'mean', ... % activation mode
-              'nphases', 8, ...
-              'norients', 8, ...
-              'niters', 1, ...  % # times to feed output back to input
-              'cpi', 3*[0.25 0.5 1 1.5 2 3 4] ...    %freqs = 0.015 * [ 6 9 12 15];
-            );
-  p        = guru_stampProps(p, varargin{:});
-
-  % Set param values that are dependent on other param values
-  if (~isfield(p, 'lambda')), p.lambda = prod(mSets.nInput)/mSets.nConns; end;
-
-  % Create the stimuli
-  X = zeros(prod(mSets.nInput)+1, length(p.cpi)*p.norients*p.nphases);
-  X(end,:) = mSets.data.train.bias;
-  for fi=1:length(p.cpi)
-
-    for oi = 1:p.norients
-      orient = pi*oi/p.norients;
-      for phsi=1:p.nphases
-        phase = 2*pi*phsi/p.nphases;
-
-        % Create the input
-        x_train = mSets.data.train.X(1:end-1,:);
-        freq = p.cpi(fi);
-        x = mfe_grating2d( p.cpi(fi), phase, orient, 0.5, mSets.nInput(1), mSets.nInput(2));
-        x_norm = x/sum(x(:));
-        %x_mean = x_norm - mean(x_norm(:)) + mean(x_train(:));
-        %x_z    = x_mean/std(x_mean(:))*std(x_train(:));
-        x_z = x_norm;
-
-        X(1:end-1,(fi-1) * p.norients * p.nphases + (oi-1) * p.nphases + phsi) = x_z(:);
-        guru_assert(~any(isnan(x_z(:))));
-      end;
-    end;
-  end;
-
-  [~, train] = de_MakeDataset('gratings', 'all', '', {mSets.data.train.opt{:}, 'nthetas', p.norients, 'nphases', p.nphases, 'cycles', p.cpi});
-  X2 = zeros(prod(mSets.nInput)+1, length(p.cpi)*p.norients*p.nphases);
-  X2(end, :) = mSets.data.train.X(end,1); %bias term
-  X2(1:end-1, :) = 0.5 + 0.5 * train.X(:, :);
-  x2_norm = X2/sum(X2(:));
-  x2_mean = x2_norm - mean(x2_norm(:));
-  x2_z    = x2_mean/std(x2_mean(:)) * std(mSets.data.train.X(:));
-  X2 = x2_z;
-  X=X2;
+  % Create a dataset to test network on.
+  [~, dset] = de_MakeDataset('gratings', 'all', '', {mSets.data.train.opt{:}, ...
+                             'nthetas', n_orients, 'nphases', n_phases, 'nfreqs', n_freqs, ...
+                             'nInput', mSets.nInput}, false, false);  % don't visualize, but do force re-creation.
+  mSets.ac.zscore = mSets.ac.zscore;
+  dset = de_NormalizeDataset(dset, mSets);
 
   % Initialize variables
-  bestofall= cell(length(mss),1);
-  avg_resp = cell(length(mss),1);
-  std_resp = cell(length(mss),1);
-  resps    = cell(length(mss),1);
+  n_sigmas = length(mss);
+  stats.settings.dset = dset;
 
-  for si=1:length(mss)
-    bestofall{si} = zeros(length(mss{si}),1);
-    avg_resp{si}  = zeros(length(mss{si}), mSets.nHidden, length(p.cpi));
-    std_resp{si}  = zeros(length(mss{si}), mSets.nHidden, length(p.cpi));
-    resps{si}     = zeros(length(mss{si}), mSets.nHidden, length(p.cpi), p.norients, p.nphases);
+  for si=1:n_sigmas
+    n_models = length(mss{si});
 
-    for mi=1:length(mss{si})
-      m = de_LoadProps(mss{si}(mi), 'ac', 'Weights');
-      m.ac.Weights(m.ac.Weights ~= 0) = abs(m.ac.Weights(m.ac.Weights ~= 0));%      0.01;
-      m.ac.XferFn = 1;
-
-      %
-      best_vals = -inf*ones(size(p.cpi));
-      best_params = zeros(length(p.cpi), 3);
-      best_x = zeros(length(p.cpi), prod(mSets.nInput));
-
-      % Run the network
-      [~,~,huo] = guru_nnExec(m.ac, X, X(1:end-1,:));
-      resps{si}(mi,:,:,:,:) = reshape(huo, [mSets.nHidden, length(p.cpi) p.norients, p.nphases]);
-
-      % normalize
-      resps_by_freq = reshape(resps{si}(mi,:,:,:,:), [mSets.nHidden, length(p.cpi) p.norients * p.nphases]);
-      resps_by_freq_norm = resps_by_freq;%/mean(abs(resps_by_freq(:)));
+    for mi=1:n_models
       fprintf('%d ', mi)
-      switch p.a_mode
-          case 'max'
-              [~,bestofall(mi)] = max(best_vals);
 
-          case {'mean','total'}
-              avg_resp{si}(mi,:,:) = mean(resps_by_freq_norm,3);
-              std_resp{si}(mi,:,:) = std(resps_by_freq_norm,[],3);
-              %best_vals = avg_resp{si}(mi,:);
-              [~,bestofall{si}(mi)] = max(avg_resp{si}(mi,:));%max(mean(mean(resps,3),2));
+      % Set up the network.
+      model = de_LoadProps(mss{si}(mi), 'ac', 'Weights');
 
-          case {'std'}
-              avg_resp{si}(mi,:,:) = mean(resps_by_freq_norm,3);
-              std_resp{si}(mi,:,:) = std(resps_by_freq_norm,[],3);
-              %best_vals = std_resp{si}(mi,:);
-              %[~,bestofall{si}(mi)] = max(std_resp{si}(mi,:));%max(mean(mean(resps,3),2));
+      % Run the network; huo==hidden unit output
+      [~,~,huo] = guru_nnExec(model.ac, dset.X, dset.X(1:end-1, :));  % uses xfer fn
+      clear('model');
 
-          case {'mean_lgs','avg_lgs', 'total_lgs'} % run through logistic
-              resps = tanh(p.lambda * resps);
-              resps_by_freq = reshape(resps, [length(p.cpi) p.norients * p.nphases]);
-              resps_by_freq_norm = resps_by_freq/mean(resps_by_freq(:));
-              avg_resp{si}(mi,:) = mean(resps_by_freq_norm,2);
-              std_resp{si}(mi,:) = std(resps_by_freq_norm,[],2);
-              %best_vals = avg_resp{si}(mi,:);
-              [~,bestofall{si}(mi)] = max(avg_resp{si}(mi,:));%max(mean(mean(resps,3),2));
+      resps = reshape(huo, [mSets.nHidden, n_freqs, n_orients, n_phases]);
+      props = {'freq', 'orient', 'phase'};  % must follow order in resps, for permute below
+      n_prop_names = length(props);
 
-          case {'std_lgs','range_lgs'} % run through logistic
-              resps = 1/(1+exp(-p.lambda * resps));
-              resps_by_freq = reshape(resps, [length(p.cpi) p.norients * p.nphases]);
-              resps_by_freq_norm = resps_by_freq/mean(resps_by_freq(:));
-              avg_resp{si}(mi,:) = mean(resps_by_freq_norm,2);
-              std_resp{si}(mi,:) = std(resps_by_freq_norm,[],2);
-              %best_vals = std_resp(mi,:);
-              [~,bestofall{si}(mi)] = max(std_resp{si}(mi,:));%max(mean(mean(resps,3),2));
+      for pi=1:n_prop_names
+        prop = props{pi};
+        n_prop_vals = eval(['n_' prop 's']);
 
-          case {'mean_sqd','avg_sqd', 'total_sqd'}
-              resps = resps.^2;
-              resps_by_freq = reshape(resps, [length(p.cpi) p.norients * p.nphases]);
-              resps_by_freq_norm = resps_by_freq/mean(resps_by_freq(:));
-              avg_resp{si}(mi,:) = mean(resps_by_freq_norm,2);
-              std_resp{si}(mi,:) = std(resps_by_freq_norm,[],2);
-              %best_vals = avg_resp(mi,:);
-              [~,bestofall{si}(mi)] = max(avg_resp{si}(mi,:));%max(mean(mean(resps,3),2));
+        % Initialize storage
+        if mi == 1
+          if si == 1
+            stats.(prop).mean = cell(n_sigmas, 1);
+            stats.(prop).std = cell(n_sigmas, 1);
+            stats.(prop).mean_best = cell(n_sigmas, 1);
+            stats.(prop).std_best = cell(n_sigmas, 1);
+          end;
+          stats.(prop).mean{si}  = zeros(n_models, mSets.nHidden, n_prop_vals);
+          stats.(prop).std{si}  = zeros(n_models, mSets.nHidden, n_prop_vals);
+          stats.(prop).mean_best{si} = zeros(n_models, n_prop_vals);
+          stats.(prop).std_best{si} = zeros(n_models, n_prop_vals);
+        end;
 
-          case {'std_sqd','range_sqd'}
-              resps = resps.^2;
-              resps_by_freq = reshape(resps, [length(p.cpi) p.norients * p.nphases]);
-              resps_by_freq_norm = resps_by_freq/mean(resps_by_freq(:));
-              avg_resp{si}(mi,:) = mean(resps_by_freq_norm,2);
-              std_resp{si}(mi,:) = std(resps_by_freq_norm,[],2);
-              %best_vals = std_resp(mi,:);
-              [~,bestofall{si}(mi)] = max(std_resp{si}(mi,:));%max(mean(mean(resps,3),2));
+        % average over the other two properties.
+        n_other_two = n_freqs * n_orients * n_phases / n_prop_vals;
+        permute_idx = [1 (1+[pi setdiff(1:n_prop_names, pi)])];
+        resps_by_prop = permute(resps, permute_idx);
+        resps_by_prop = reshape(resps_by_prop, [mSets.nHidden, n_prop_vals, n_other_two]);
 
-          case 'range',
-              rng  = max(alld') - min(alld');
-              [~,bestofall(mi)] = max(rng);
-
-          otherwise, error('%s nyi', p.a_mode);
+        stats.(prop).mean{si}(mi,:,:) = mean(resps_by_prop, 3);
+        stats.(prop).std{si}(mi,:,:) = std(resps_by_prop, [], 3);
+        [~, stats.(prop).mean_best{si}(mi)] = max(stats.(prop).mean{si}(mi,:));
+        [~, stats.(prop).std_best{si}(mi)] = max(stats.(prop).std{si}(mi,:));
       end;
     end;
   end;
 
-  stats.avg_resp = avg_resp;
-  stats.std_resp = std_resp;
-  stats.settings = p;
- 
