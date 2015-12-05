18dec5817f7db46fe124c2a7382e1bc0c1bdb871
bcipolli <bcipolli@ucsd.edu>
2015-12-04 18:16:51 -0800

    STY: cleaning sergent scripts.

--- experiments/34x25/sergent_1982/classic_sergent_sergent.m
+++ experiments/34x25/sergent_1982/classic_sergent_sergent.m
@@ -1,5 +1,4 @@
 % Model run with targets and distracter sets swapped
-clear all variables;
 
 [args,opts] = classic_sergent_args();
-[mSets, models, stats] = de_Simulator('sergent_1982', 'de', 'sergent', {opts{:}}, args{:});
+[mSets, models, stats] = de_Simulator('sergent_1982', 'de', 'sergent', opts, args);
--- experiments/34x25/sergent_1982/uber_sergent_sergent.m
+++ experiments/34x25/sergent_1982/uber_sergent_sergent.m
@@ -1,10 +1,4 @@
-clear all variables; clear all globals;
-
-%stats = {'ipd', 'ffts', 'distns', 'pca', 'images'};
-stats = {}; %'images', 'ffts'};
-plts = {'ls-bars', stats{:}};
-
-[args,opts]  = uber_sergent_args('plots', plts, 'stats', stats, 'runs', 50);
+[args,opts]  = uber_sergent_args();
 
 % Run sergent task by training on all images
 [trn, tst] = de_SimulatorUber('vanhateren/250', 'sergent_1982/de/sergent', opts, args);
