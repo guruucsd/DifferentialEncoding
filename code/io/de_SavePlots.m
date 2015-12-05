41cf9df6b877607c3849e51fb43eedcadd0555dc
bcipolli <bcipolli@ucsd.edu>
2015-12-05 13:15:00 -0800

    ENH: resize plots to be big when saving.

--- code/io/de_SavePlots.m
+++ code/io/de_SavePlots.m
@@ -6,28 +6,27 @@ function out = de_SavePlots(mSets, figs)
 
   % Loop over figure names to make sure they're all unique.
   figNames = {figs.name};
-  [b,i,j] = unique(figNames);
+  [~, ~, idx] = unique(figNames);
 
-  for f=1:length(figs)
-    nSameName = length(find(j==j(f)));
-    if (nSameName > 1)
-      figs(f).name = sprintf('%s-%d', figs(f).name, 1+length(find(j(1:f-1)==j(f))));
-    end;
+  for fi=1:length(figs)
+    nSameName = length(find(idx==idx(fi)));
+    if sum(idx==idx(fi)) == 1, continue; end;
+    figs(fi).name = sprintf('%s-%d', figs(fi).name, 1 + sum(idx(1:fi-1)==idx(fi)));
   end;
 
   % Loop over each output type
-  for i=1:length(out.plots)
-    ext = [ '.' out.plots{i} ];
+  for pi=1:length(out.plots)
+    ext = [ '.' out.plots{pi} ];
 
     % Loop over each plot
-    for j=1:length(figs)
-      out.files{end+1} = de_GetOutFile(mSets, 'plot', figs(j).name, ext, mSets.runs);
+    for fi=1:length(figs)
+      out.files{end+1} = de_GetOutFile(mSets, 'plot', figs(fi).name, ext, mSets.runs);
 
       % Don't re-create existing figures
-      if (figs(j).cached), continue; end;
+      if (figs(fi).cached), continue; end;
 
-      if (ismember(1,mSets.debug))
-        fprintf('Saving plot %-15s to %s.\n', ['"' figs(j).name '"'], out.files{end});%guru_replace(out.files{end}, de_GetBaseDir(), ''));
+      if (ismember(1, mSets.debug))
+        fprintf('Saving plot %-15s to %s.\n', ['"' figs(fi).name '"'], out.files{end});
       end;
 
       %i/o
@@ -35,8 +34,18 @@ function out = de_SavePlots(mSets, figs)
         guru_mkdir(guru_fileparts(out.files{end}, 'pathstr'));
       end;
 
-      % export_fig(figs(j).handle, [out.files{end} '-export-fig' ext], '-transparent');
-      saveas(figs(j).handle, out.files{end}, ext(2:end));
+    figure(figs(fi).handle);
+      cur_pos = get(figs(fi).handle, 'position');
+      aspect_ratio = cur_pos(3)/cur_pos(4);
+      set(figs(fi).handle, 'paperunits', 'inches');
+      set(figs(fi).handle, 'paperposition', [0 0 12 12/aspect_ratio]);
+      if strcmp(ext, '.fig')
+        saveas(figs(fi).handle, out.files{end}, ext(2:end));
+      else
+        %export_fig(figs(fi).handle, [out.files{end} '-export-fig' ext], '-transparent');
+        print(figs(fi).handle, out.files{end}, ['-d' ext(2:end)]);
+      end;
+      set(figs(fi).handle, 'units', 'pixels');
 
       % Make a version of the plot for publication
       if (mSets.out.pub)
@@ -44,7 +53,7 @@ function out = de_SavePlots(mSets, figs)
         if (~strcmp(ft, 'fig'))
           hatch_fn = [out.files{end}(1:end-4) '-pub' out.files{end}(end-3:end)];
           if (ismember(1,mSets.debug))
-            fprintf('Saving plot %-15s to %s.\n', ['"' figs(j).name '"'], hatch_fn);%guru_replace(hatch_fn, de_GetBaseDir(), ''));
+            fprintf('Saving plot %-15s to %s.\n', ['"' figs(fi).name '"'], hatch_fn);
           end;
 
           im = imread(out.files{end}, ft);
