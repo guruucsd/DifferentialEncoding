dfbcaf2abe466647946d8f93b744c0cf661ca211
Ben Cipollini <bcipolli@ucsd.edu>
2015-12-07 07:38:24 -0800

    BF: Christman analyzer script was missing :o

---
+++ code/_expts/christman_etal_1991/de_Analyzer.m
@@ -0,0 +1,19 @@
+function [stats, figs]  = de_AnalyzerKit(mSets, mss)
+%function [stats, figs]  = de_AnalyzerKit(mSets, mss)
+%
+% Trains a differential encoder under the model and training parameters specified
+%
+% Inputs:
+% models   : resulting models after training
+%
+% Outputs:
+% stats      :
+% figs       :
+
+  [stats,figs] = de_AnalyzerDE(mSets, mss);
+%  [stats.raw.r] = de_FindRejectionsKit(mss, mSets.rej, stats.raw, stats.raw.r);
+  [mss]        = de_DoRejections(mss, stats.raw.r);
+
+  % Do Kit-specific stats & figs
+  [stats.rej.sf] = de_StaticizerRF(mSets, mss, stats);
+  [figs]         = [figs de_FigurizerRF(mSets, mss, stats) ];
