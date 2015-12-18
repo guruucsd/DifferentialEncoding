1a85ddb0781b0cfea0578892483d69c6de0610f6
Ben Cipollini <bcipolli@ucsd.edu>
2015-12-17 21:00:36 -0800

    STY: removing unneccessary scripts.

--- experiments/68x50/sergent_1982/uber_sergent_stacked.m
+++
@@ -1,9 +0,0 @@
-clear all variables; clear all globals;
-
-stats = {}; % 'images','ffts', 'distns', 'ipd'};
-plts = {stats{:}};
-
-[args,opts] = uber_sergent_args('plots', plts,'stats', stats, 'runs', 50);
-
-% Run sergent task by training on all images
-[trn, tst] = de_SimulatorUber('vanhateren/250', 'sergent_1982/de/sergent', opts, args);
