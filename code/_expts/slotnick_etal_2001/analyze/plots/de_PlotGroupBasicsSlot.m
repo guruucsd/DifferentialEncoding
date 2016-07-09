d0e9efbe8402e144059a272781ef79b3636e6b37
Vishaal Prasad <vishaal.prasad@gmail.com>
2016-07-09 12:32:40 -0700

    Fix runtime bugs; still has bug in comparison chart

--- code/_expts/slotnick_etal_2001/analyze/plots/de_PlotGroupBasicsSlot.m
+++ code/_expts/slotnick_etal_2001/analyze/plots/de_PlotGroupBasicsSlot.m
@@ -4,39 +4,42 @@ function figs = de_PlotGroupBasicsSlot( ms, ss )
     models = {ms.cate; ms.coor};
     ds = 'test';
     num_ttypes = size(data, 1);
-    trial_types = data{1}.rej.cc.perf.(ds){1}.trial_types;
+    trial_types = data{1}.rej.cc.anova.(ds).trial_types;
 
     for tri = 1:length(trial_types)  % trial type
         trial_type = trial_types{tri};
-        fig = de_NewFig(sprintf('blobdot_comparison_%s', trial_type);
+        fig = de_NewFig(sprintf('blobdot_comparison_%s', trial_type));
         yrng = 0;
 
         for tai = 1:length(data)  % task type
-            mSets = models{tt}(1);
+            mSets = models{tai}(1);
             taskTitle = guru_capitalizeStr(mSets.data.taskType);
             stimSet = mSets.data.stimSet;
 
-            right = data{tt}.rej.cc.perf.(ds){1}{tri};
-            left = data{tt}.rej.cc.perf.(ds){end}{tri};
+            right = data{tai}.rej.cc.perf.(ds){1}{tri};
+            left = data{tai}.rej.cc.perf.(ds){end}{tri};
             runs = min(length(models{1}), length(models{end}));
 
             left_mean = mean(left(:));
             right_mean = mean(right(:));
             left_stderr = std(mean(left, 2)) / sqrt(size(left, 1));
-            right_stderr = std(mean(left, 2)) / sqrt(size(right, 1));
+            right_stderr = std(mean(right, 2)) / sqrt(size(right, 1));
 
-            ax(tt) = subplot(1, num_ttypes, tt);
+            ax(tai) = subplot(1, num_ttypes, tai);
             de_CreateSlotnickFigure1([left_mean right_mean], ...
                                      [left_stderr right_stderr], ...
-                                     taskTitle, stimSet, runs, ax(tt));
+                                     taskTitle, stimSet, runs, ax(tai));
 
             % Store properties to correct axes
-            avg(tt) = mean([left_mean, right_mean]);
+            avg(tai) = mean([left_mean, right_mean]);
             yrng = max(yrng, abs(left_mean - right_mean));
         end
 
-        for tt = 1:num_ttypes
+        for tai = 1:num_ttypes
             % Only need yrng/2 for full range, but 2* yrng for room for error bar
-            set(ax(tt), 'ylim', avg(tt) + 2*[-1, 1])
+            set(ax(tai), 'ylim', avg(tai) + 2*[-1, 1])
         end
     end;
+    
+    figs = [fig];
+
