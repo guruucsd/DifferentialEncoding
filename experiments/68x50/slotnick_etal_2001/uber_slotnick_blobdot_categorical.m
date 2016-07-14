808332633f32dd55e275ddf14ba19415b77a5add
Vishaal Prasad <vishaal.prasad@gmail.com>
2016-07-13 21:32:06 -0700

    Make 68x50, 34x25 exprs get respective args

--- experiments/68x50/slotnick_etal_2001/uber_slotnick_blobdot_categorical.m
+++ experiments/68x50/slotnick_etal_2001/uber_slotnick_blobdot_categorical.m
@@ -1,3 +1,11 @@
+%Add current directory to top of path, so the correct uber_slotnick_args is 
+%referenced. This only works if ubser_slotnic_args and this file are in
+%the same directory
+
+fname = mfilename('fullpath');
+[dir, ~, ~] = fileparts(fname);
+addpath(dir);
+
 [args, opts]  = uber_slotnick_args();
 
 % Run sergent task by training on all images
