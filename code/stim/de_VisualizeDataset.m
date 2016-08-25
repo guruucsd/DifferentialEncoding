75548c647c228ab45d411a6ab99d2af1ba836392
Vishaal Prasad <vishaal.prasad@gmail.com>
2016-08-24 17:40:44 -0700

    Fix visualization by subtracting min

--- code/stim/de_VisualizeDataset.m
+++ code/stim/de_VisualizeDataset.m
@@ -12,10 +12,17 @@ function fig = de_visualizeData(dset, nImages)
   fig = de_NewFig('dataset-images', '__img', img_shape, nImages);
   set(fig.handle, 'Position', [0, 0, 1200, 1200]);
 
-  for ii=1:min(4*4, length(im2show)) %make sure only 14 are shown
+  for ii=1:min(4*4, length(im2show)) %make sure only 16 are shown
       subplot(4, 4, ii);
       colormap gray;
-      imagesc( reshape(dset.X(1:n_pix, im2show(ii)), img_shape), [0, 1]);
+      img = reshape(dset.X(1:n_pix, im2show(ii)), img_shape);
+      img_min = min(img);
+      if ~isscalar(img_min) %make sure that it is the global min 
+          img_min = min(img_min);
+      end
+      
+      img = img - img_min; % smallest value is 0
+      imagesc( img, [0, 1]);
       axis image; set(gca, 'xtick',[],'ytick',[]);
       lbl = dset.XLAB{im2show(ii)};
       if isfield(dset, 'TLAB')
