72ac6b71c0e00e930924962c8dac2e26bd91c6e0
Vishaal Prasad <vishaal.prasad@gmail.com>
2016-09-06 15:28:23 -0700

    Fix visualization by normalizing half the images

--- code/stim/de_VisualizeDataset.m
+++ code/stim/de_VisualizeDataset.m
@@ -17,16 +17,24 @@ function fig = de_visualizeData(dset, nImages)
       colormap gray;
       img = reshape(dset.X(1:n_pix, im2show(ii)), img_shape);
       img_min = min(img);
-      if ~isscalar(img_min) %make sure that it is the global min 
+      img_max = max(img);
+      if ~isscalar(img_min) %make sure that it is the global min/max
           img_min = min(img_min);
+          img_max = min(img_max);
       end
       
       img = img - img_min; % smallest value is 0
+      if (ii>min(4*4/2, length(im2show)/2))
+          img = img / (img_max - img_min);
+      end
       imagesc( img, [0, 1]);
       axis image; set(gca, 'xtick',[],'ytick',[]);
       lbl = dset.XLAB{im2show(ii)};
       if isfield(dset, 'TLAB')
           lbl = sprintf('%s\n%s', lbl, dset.TLAB{im2show(ii)});
+          if (ii>min(4*4/2, length(im2show)/2))
+              ylabel('normalized')
+          end
       end;
       xlabel(guru_text2label(lbl));
   end;
