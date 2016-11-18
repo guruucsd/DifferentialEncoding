072a395bb933777f9d3194502962bbd20b671671
Ben Cipollini <bcipolli@ucsd.edu>
2016-11-18 11:48:08 -0800

    ENH: generalize bandpass_by_task into a callable function.

--- experiments/bandpass/bandpass_by_task.m
+++ experiments/bandpass/bandpass_by_task.m
@@ -1,42 +1,48 @@
-scale=2; % if blob-dot, scale=2. else scale=1
+function bandpass_by_task(n_steps, window_steps, args_path, task_info, extra_opts, extra_args)
+  if ~exist('extra_opts', 'var'), extra_opts = {}; end;
+  if ~exist('extra_args', 'var'), extra_args = {}; end;
 
-imgsiz = [34, 25] * scale;
-bandpass_width = 4;
-bandpass_step = 1 * scale;
+  % Get args & opts from function
+  [dir_name, file_name] = fileparts(args_path);
+  addpath(dir_name);
+  args_fn = str2func(file_name);
+  [args, opts] = args_fn();
+  args = [args, extra_args];
+  opts = [opts, extra_opts];
 
-scriptname = 'slotnick_etal_2001/blob-dot/categorical';
+  % Pre-train the autoencoder on full-fidelity. This will get us the image size.
+  [trn] = de_SimulatorUber('vanhateren/250', '', opts, args);
+  max_freq = max(trn.mSets.nInput / 2);
+  bandpass_step =  max_freq / n_steps;
+  window_size = bandpass_step * window_steps;
 
-low = 0;
-high = low + bandpass_width;
+  % Train the classifier on the bandpass-filtered stimuli.
+  tmp = linspace(0, max_freq, n_steps + 2);
+  center_freqs = tmp(2:end-1);
+  low_freqs = max(0, center_freqs - window_size/2);
+  high_freqs = min(max_freq, center_freqs + window_size/2);
 
-ds = 'test';
-center_freq = [];
-results = [];
-errors = [];
-[args, opts]  = uber_slotnick_args('p.dropout', 0.7, 'sigma', [10, 10]);
-orig_opts = opts;
+  means = zeros(length(trn.mSets.sigma), length(center_freqs));
+  stderrs = zeros(size(means));
 
-while high <= max(imgsiz)/2
-    opts = [orig_opts, 'bandpass', [low, high]];
-    [trn, tst] = de_SimulatorUber('vanhateren/250', scriptname, opts, args);
-    stats = tst.stats;
-    left = stats.rej.cc.perf.(ds){end};
-    runs = size(left{1});
-    runs = runs(1);
-    avg = mean(cellfun(@(d) mean(d(:)), left)); %For this script, assume sigmas are same
-    results = [results, avg];
-    center_freq = [center_freq, (low + high)/2];
-    stddev = std2([left{1}, left{2}]);
-    errors = [errors, stddev/sqrt(runs)];
-    low = low + bandpass_step;
-    high = high + bandpass_step;
+  for si=1:n_steps
+    % Train the model
+    cur_opts = struct( ...
+        'uber', {opts}, ...
+        'task', {[opts, 'bandpass', [low_freqs(si), high_freqs(si)]]} ...
+    );
+    [~, tst] = de_SimulatorUber('vanhateren/250', task_info, cur_opts, args);
     close all;
 
+    % Store mean performance
+    means(:, si) = cellfun(@(x) mean(x), tst.stats.rej.p.err.vals);
+    stderrs(:, si) = cellfun(@(x) std(x), tst.stats.rej.p.err.vals) / sqrt(tst.mSets.runs);
 end
 
 figure;
-errorbar(center_freq, results, errors);
-xlabel('Center Frequency (cycles/ img)');
-ylabel('Sum Squared Error');
-title(sprintf('SSE vs Frequency: Bandpass Step %d, Bandpass Width %d', ...
-    bandpass_step, bandpass_width));
+errorbar(repmat(center_freqs, [2, 1])', (means'), (stderrs'), 'LineWidth', 2.0);
+set(gca, 'FontSize', 14);
+legend(guru_csprintf('%.2f', num2cell(trn.mSets.sigma)), 'FontSize', 12);
+xlabel('Center Frequency (cycles/ img)', 'FontSize', 14);
+ylabel('Sum Squared Error', 'FontSize', 14);
+title(sprintf('SSE vs Frequency; Bandpass Step=%.2f, Width=%.2f', bandpass_step, window_size), 'FontSize', 16);
