14448cb4a9c9fca351f23fe95982db66f6751b96
Vishaal Prasad <vishaal.prasad@gmail.com>
2017-02-17 17:47:13 -0800

    Parameters used for COGSCI submission

--- experiments/68x50/slotnick_etal_2001/uber_slotnick_plusminus_all.m
+++
@@ -1,8 +0,0 @@
-uber_slotnick_plusminus_categorical;
-tst_cate = tst;
-
-uber_slotnick_plusminus_coordinate;
-tst_coord = tst;
-
-% We should model code after: kitterle_interaction_analysis(tst_freq, tst_type);
-slotnick_interaction_analysis(tst_cate, tst_coord);
